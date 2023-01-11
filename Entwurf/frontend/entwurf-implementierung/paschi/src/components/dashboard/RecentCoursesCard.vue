22d3c1df8dcbc5b4b555eb018ee22213dd86bc6f
uezbf <uezbf@student.kit.edu>
2023-01-11 14:26:25 +0100

    update Diagram

--- Entwurf/frontend/entwurf-implementierung/paschi/src/components/dashboard/RecentCoursesCard.vue
+++ Entwurf/frontend/entwurf-implementierung/paschi/src/components/dashboard/RecentCoursesCard.vue
@@ -1,10 +1,15 @@
 <template>
-$END$
+  <v-list>
+    <v-list-item v-for="course, key in courses">
+      <v-list-item-title>{{course.name}}</v-list-item-title>
+      <v-btn @click="navigateTo(course)"></v-btn>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script>
 export default {
-name: "RecentCoursesCard"
+  name: "RecentCoursesCard"
 }
 </script>
 
