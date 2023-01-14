d8e6968e3191590e736b91b4d0d85238e38efaff
Florian Knechtel <fj.knechtel@gmail.com>
2023-01-14 20:43:39 +0100

    Initial Spring

---
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/PAschIApplication.java
@@ -0,0 +1,13 @@
+package edu.kit.informatik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PAschIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PAschIApplication.class, args);
+	}
+
+}
---
+++ Implementierung/Backend/src/test/java/edu/kit/informatik/PAschIApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kit.informatik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PAschIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
