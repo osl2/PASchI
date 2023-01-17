8551086cc677569687b27487217cc21c2e8cd3e2
David <uopmx@student.kit.edu>
2023-01-17 16:08:37 +0100

    editStudentPage

--- Implementierung/frontend/paschi/src/components/studentManagement/EditStudentPage.vue
+++ Implementierung/frontend/paschi/src/components/studentManagement/EditStudentPage.vue
@@ -2,9 +2,32 @@
   <navigation-bar>
     <template v-slot:extension>
       <v-btn>speichern</v-btn>
+      <v-btn>Schüler löschen</v-btn>
     </template>
   </navigation-bar>
-
+  <v-main>
+    <v-container>
+      <v-text-field
+        v-model="preName"
+        label="Vorame"
+        type="input"
+      ></v-text-field>
+      <v-text-field
+        v-model="lastName"
+        label="Nachname"
+        type="input"
+      ></v-text-field>
+    </v-container>
+    <v-dialog
+      v-model="deleteSudentDialog"
+    >
+      <v-card>
+        "Delete student?"
+        <v-btn>cancel</v-btn>
+        <v-btn>confirm</v-btn>
+      </v-card>
+    </v-dialog>
+  </v-main>
 </template>
 
 <script>
@@ -12,7 +35,15 @@ import NavigationBar from "@/components/navigation/NavigationBar.vue";
 
 export default {
   name: "editStudentPage.vue",
-  components: {NavigationBar}
+  components: {
+  NavigationBar
+  },
+  props:['studentId'],
+  data: () => ({
+    preName: 'Gregor',
+    lastName: 'Snelting',
+    deleteSudentDialog: true,
+  }),
 }
 </script>
 
