a3009ef58916d8a7ebe782b5176fa3f48531b166
David <uopmx@student.kit.edu>
2023-01-17 15:20:41 +0100

    Package student Management added

---
+++ Implementierung/frontend/paschi/src/components/studentManagement/EditStudentPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <navigation-bar>
+    <template v-slot:extension>
+      <v-btn>speichern</v-btn>
+    </template>
+  </navigation-bar>
+
+</template>
+
+<script>
+import NavigationBar from "@/components/navigation/NavigationBar.vue";
+
+export default {
+  name: "editStudentPage.vue",
+  components: {NavigationBar}
+}
+</script>
+
+<style scoped>
+
+</style>
--- Implementierung/frontend/paschi/src/plugins/router.ts
+++ Implementierung/frontend/paschi/src/plugins/router.ts
@@ -1,10 +1,14 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Login from "@/components/authentication/Login.vue";
 import Dashboard from "@/components/dashboard/Dashboard.vue";
+import editStudentPage from "@/components/studentManagement/EditStudentPage.vue";
+import EditStudentPage from "@/components/studentManagement/EditStudentPage.vue";
 
 const routes = [
   {path: '/login', name: 'Login', component: Login},
-  {path: '/dashboard', name: 'Dashboard', component: Dashboard}
+  {path: '/dashboard', name: 'Dashboard', component: Dashboard},
+  {path: '/editStudent', name: 'EditStudentPage', component: EditStudentPage},
+
 ]
 
 const router = createRouter({
