c6450acde3533e0fb8905af43c849ef477f3ee4a
Florian Knechtel <fj.knechtel@gmail.com>
2023-01-17 11:15:32 +0100

    Added JavaDoc

--- Implementierung/Backend/src/main/java/edu/kit/informatik/controller/BaseController.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/controller/BaseController.java
@@ -4,24 +4,64 @@ import edu.kit.informatik.service.BaseService;
 
 import java.util.List;
 
+/**
+ * Abstrakte Klasse BaseController zur Vererbung der Methoden für alle benötigten Controller.
+ * Controller dienen zum Bauen der REST-Api.
+ *
+ * @param <Entity> Entität des Controllers
+ * @param <Dto> Dto der Entität
+ *
+ * @author ugqbo
+ * @version 1.0
+ */
 public abstract class BaseController<Entity, Dto> {
 
+    /**
+     * Allgemeiner {@link BaseService Service}
+     */
     BaseService<Entity, Dto> service;
 
-    public Dto add(Dto dto){
+    /**
+     * REST-Api zum Hinzufügen der Entität
+     * @param dto Dto der Entität
+     * @return Dto der Entität
+     */
+    public Dto add(Dto dto) {
         return this.service.add(dto);
     }
 
+    /**
+     * REST-Api zum Aktualisieren der Entität
+     * @param dto Dto der Entität
+     * @return Dto der Entität
+     */
     public Dto update(Dto dto) {
         return this.service.update(dto);
     }
-    public Dto getById(long id){
+
+    /**
+     * REST-Api zur Rückgabe einer Entität
+     * @param id Id der Entität
+     * @return Dto der Entität
+     */
+    public Dto getById(long id) {
         return this.service.getById(id);
     }
-    public List<Dto> getAll(){
+
+    /**
+     * REST-Api Rückgabe aller Entitäten
+     * @return Liste der Dtos der Entitäten
+     */
+    public List<Dto> getAll() {
         return this.service.getAll();
     }
-    public long delete(long id){
+
+    /**
+     * REST-Api zum Löschen einer Entität
+     * @param id Id der Entität
+     * @return Dto der Entität
+     */
+    public long delete(long id) {
         return this.service.delete(id);
     }
 
