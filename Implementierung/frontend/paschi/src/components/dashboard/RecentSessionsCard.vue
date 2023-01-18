a78ec143691afe111bc93127394ca6697b072003
uezbf <uezbf@student.kit.edu>
2023-01-18 18:05:52 +0100

    Add RecentCourseCard

---
+++ Implementierung/frontend/paschi/src/components/dashboard/RecentSessionsCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card color="primary">
+    <v-card-title>
+      <v-row class="ma-2">
+        Letzte Sitzungen
+      </v-row>
+    </v-card-title>
+    <v-card-item v-for="session in sessions">
+      {{session.name}}
+      <v-btn @click="navigateTo(session)"></v-btn>
+    </v-card-item>
+  </v-card>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import router from "@/plugins/router";
+
+export default defineComponent({
+  name: "RecentSessionsCard",
+  setup() {
+
+    return {
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
