d34a22436c6974ea32a111610f91b6ef887e85de
uekai <luka.kosak@student.kit.edu>
2023-01-18 18:41:23 +0100

    Implemented Store Classes

--- Implementierung/frontend/paschi/src/store/StudentStore.ts
+++ Implementierung/frontend/paschi/src/store/StudentStore.ts
@@ -7,16 +7,25 @@ export const useStudentStore = defineStore('students', {
   }),
   actions: {
     addStudent(student: Student) {
-
+      this.students.push(student);
     },
     deleteStudent(id: string) {
-
+      this.students.forEach((element, index) => {
+        if (element.id === id) {
+          this.students.splice(index, 1);
+        }
+      });
     },
-    getStudent(id: string): Student {
-
+    getStudent(id: string): Student | undefined {
+      this.students.forEach((element) => {
+        if (element.id === id) {
+          return element;
+        }
+      });
+      return undefined;
     },
     getAllStudents(): Student[] {
-
+      return this.students;
     }
   }
 })
