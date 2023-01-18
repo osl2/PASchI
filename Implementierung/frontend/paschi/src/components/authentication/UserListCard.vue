a336d60c44ac8e6fcf8a8211bfca354b41d39e73
Aaron Sutor <unvil@student.kit.edu>
2023-01-18 19:49:12 +0100

    Vue Komponente für ausgelagerte Listen der Admin Page erstellt

--- Implementierung/frontend/paschi/src/components/authentication/UserListCard.vue
+++ Implementierung/frontend/paschi/src/components/authentication/UserListCard.vue
@@ -2,11 +2,12 @@
   <v-card color="primary">
     <v-card-title>
       <v-row class="ma-2">
-        Neue, noch nicht bestätigte Accounts:
+        Bereits akzeptierte Accounts:
       </v-row>
     </v-card-title>
-    <v-card-item v-for="request in requests">
-      {{request.name}}
+    <v-card-item v-for="user in users">
+      {{user.name}}
+      <!-- TODO Hier kommt ein delte-Icon hin -->
       <v-btn @click="navigateTo(course)"></v-btn>
     </v-card-item>
   </v-card>
@@ -17,7 +18,7 @@ import {defineComponent} from "vue";
 import router from "@/plugins/router";
 
 export default defineComponent({
-  name: "RequestListCard",
+  name: "UserListCard",
 })
 </script>
 
