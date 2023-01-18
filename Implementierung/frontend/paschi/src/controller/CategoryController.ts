da94785a4d27a9a43aadcd97a85db276038a8a37
uekai <luka.kosak@student.kit.edu>
2023-01-18 15:48:17 +0100

    Added CategoryController.ts

--- Implementierung/frontend/paschi/src/controller/CategoryController.ts
+++ Implementierung/frontend/paschi/src/controller/CategoryController.ts
@@ -1,3 +1,34 @@
+import {Quality} from "@/model/userdata/interactions/Quality";
+import {Category} from "@/model/userdata/interactions/Category";
+
 export class CategoryController {
-  
+
+  private static controller: CategoryController = new CategoryController();
+
+  private constructor() {
+  }
+
+  static getCategoryController(): CategoryController {
+    return CategoryController.controller;
+  }
+
+  createCategory(name: string): number {
+    return 0;
+  }
+
+  createRatedCategory(name: string, quality: Quality): number {
+    return 0;
+  }
+
+  deleteCategory(id: number) {
+
+  }
+
+  updateCategory(id: number, name: string, quality: Quality) {
+
+  }
+
+  getCategories(): Category[] {
+    return [];
+  }
 }
