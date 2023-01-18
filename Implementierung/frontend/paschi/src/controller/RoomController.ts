e68a206016b3fae29d1c497d161fe6a3bbd9ef92
uekai <luka.kosak@student.kit.edu>
2023-01-18 16:41:57 +0100

    Added RoomController.ts

--- Implementierung/frontend/paschi/src/controller/RoomController.ts
+++ Implementierung/frontend/paschi/src/controller/RoomController.ts
@@ -1,3 +1,46 @@
+import {Room} from "@/model/userdata/rooms/Room";
+import {Position} from "@/model/userdata/rooms/Position";
+
 export class RoomController {
-  
+
+  private static controller: RoomController = new RoomController();
+
+  private constructor() {
+  }
+
+  static getRoomController(): RoomController {
+    return RoomController.controller;
+  }
+
+  createRoom(name: string): number {
+    return 0;
+  }
+
+  updateRoom(id: number) {
+
+  }
+
+  deleteRoom(id: number) {
+
+  }
+
+  getRoom(id: number): Room | undefined {
+    return undefined;
+  }
+
+  getAllRooms(): Room[] {
+    return [];
+  }
+
+  addChair(roomId: number, pos: Position): number {
+    return 0;
+  }
+
+  addTable(roomId: number, pos: Position, length: number, width: number): number {
+    return 0;
+  }
+
+  removeRoomObject(roomId: number, objectId: number) {
+
+  }
 }
