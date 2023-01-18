b75da4df7fe243b7af2f33afce670fe0859cbaf9
Florian Knechtel <fj.knechtel@gmail.com>
2023-01-18 19:52:11 +0100

    Added Annotations

--- Implementierung/Backend/src/main/java/edu/kit/informatik/model/userdata/rooms/Position.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/model/userdata/rooms/Position.java
@@ -1,10 +1,19 @@
 package edu.kit.informatik.model.userdata.rooms;
 
 import edu.kit.informatik.model.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Position {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+    @ManyToOne
     private User user;
     private double xCoordinate;
     private double yCoordinate;
@@ -14,6 +23,10 @@ public class Position {
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
         this.orientation = orientation;
+    }
+
+    public Position() {
+
     }
 
     public String getId() {
