e3807934eb83c517696aec5017d4ba207458c232
uekai <luka.kosak@student.kit.edu>
2023-01-18 16:42:28 +0100

    Added UserController.ts

--- Implementierung/frontend/paschi/src/controller/UserController.ts
+++ Implementierung/frontend/paschi/src/controller/UserController.ts
@@ -1,3 +1,38 @@
+import {User} from "@/model/User";
+
 export class UserController {
-  
+
+  private static controller: UserController = new UserController();
+
+  private constructor() {
+  }
+
+  static getUserController(): UserController {
+    return UserController.controller;
+  }
+
+  login(email: string, password: string) {
+
+  }
+
+  register(firstName: string, lastName: string, email: string, password: string, repeatPassword: string) {
+
+  }
+
+  update(firstName: string, lastName: string, email: string, password: string) {
+
+  }
+
+  changePassword(oldPassword: string, newPassword: string, repeatPassword: string) {
+
+  }
+
+  getUser(): User | undefined {
+    // undefined entfernen nachdem implementiert
+    return undefined
+  }
+
+  delete() {
+
+  }
 }
