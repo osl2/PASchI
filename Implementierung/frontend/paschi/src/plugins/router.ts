2d4e942ccfda4ba08d0501e349910ec70e56d737
Aaron Sutor <unvil@student.kit.edu>
2023-01-18 19:30:15 +0100

    AdminPage erstellt Login Page um Button ergänzt

--- Implementierung/frontend/paschi/src/plugins/router.ts
+++ Implementierung/frontend/paschi/src/plugins/router.ts
@@ -4,10 +4,12 @@ import Dashboard from "@/components/dashboard/Dashboard.vue";
 import editStudentPage from "@/components/studentManagement/EditStudentPage.vue";
 import EditStudentPage from "@/components/studentManagement/EditStudentPage.vue";
 import Register from "@/components/authentication/Register.vue";
+import AdminPage from "@/components/authentication/AdminPage.vue";
 
 const routes = [
   {path: '/login', name: 'Login', component: Login},
   {path: '/register', name: 'Register', component: Register},
+  {path: '/admin', name: 'AdminPage', component: AdminPage},
   {path: '/dashboard', name: 'Dashboard', component: Dashboard},
   {path: '/editStudent', name: 'EditStudentPage', component: EditStudentPage},
 
