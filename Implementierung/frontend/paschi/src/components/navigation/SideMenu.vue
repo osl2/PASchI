1cbd20249e7e935e198cbef918d714a7fcfaaaeb
uezbf <uezbf@student.kit.edu>
2023-01-18 19:50:40 +0100

    Add SideMenu

---
+++ Implementierung/frontend/paschi/src/components/navigation/SideMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-navigation-drawer expand-on-hover rail elevation="12" color="primary">
+    <v-list nav>
+      <v-list-item prepend-icon="fas fa-tachometer-alt" @click="router.push('/dashboard')" >
+        <v-list-item-title>
+          Dashboard
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "SideMenu",
+  methods: {
+    Dashboard() {
+      return Dashboard
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
