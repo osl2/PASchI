52ceb8fb6a87ea2f23e3ccbed2f81ea206d27ad2
uekai <luka.kosak@student.kit.edu>
2023-01-18 16:42:18 +0100

    Added StudentController.ts

--- Implementierung/frontend/paschi/src/controller/StudentController.ts
+++ Implementierung/frontend/paschi/src/controller/StudentController.ts
@@ -1,3 +1,59 @@
+import {Student} from "@/model/userdata/interactions/Student";
+import {Course} from "@/model/userdata/courses/Course";
+import {Interaction} from "@/model/userdata/interactions/Interaction";
+
 export class StudentController {
-  
+
+  private static controller: StudentController = new StudentController();
+
+  private constructor() {
+  }
+
+  static getStudentConroller(): StudentController {
+    return StudentController.controller;
+  }
+
+  createStudent(firstName: string, lastName: string): number {
+    return 0;
+  }
+
+  updateStudent(id: number, firstName: string, lastName: string) {
+
+  }
+
+  deleteStudent(id: number) {
+
+  }
+
+  getStudent(id: number): Student | undefined {
+    return undefined;
+  }
+
+  getAllStudents(): Student[] {
+    return [];
+  }
+
+  getCoursesOfStudent(studentId: number): Course[] {
+    return [];
+  }
+
+  addCourseToStudent(studentId: number, courseId: number) {
+
+  }
+
+  removeCourseFromStudent(studentId: number, courseId: number) {
+
+  }
+
+  getInteractionsOfStudent(studentId: number): Interaction[] {
+    return [];
+  }
+
+  addInteraction(studentId: number, interactionId: number) {
+
+  }
+
+  removeInteraction(studentId: number, interactionId: number) {
+
+  }
 }
