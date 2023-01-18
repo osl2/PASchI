6f891d0bf2f89e4d28a3902b79ef45148cc56169
uekai <luka.kosak@student.kit.edu>
2023-01-18 16:42:06 +0100

    Added StatsController.ts

--- Implementierung/frontend/paschi/src/controller/StatsController.ts
+++ Implementierung/frontend/paschi/src/controller/StatsController.ts
@@ -1,3 +1,27 @@
 export class StatsController {
 
+  private static controller: StatsController = new StatsController();
+
+  private constructor() {
+  }
+
+  static getStatsController(): StatsController {
+    return StatsController.controller;
+  }
+
+  getStudentStats(studentId: number) {
+
+  }
+
+  getCourseStats(courseId: number) {
+
+  }
+
+  getSessionStats(sessionId: number) {
+
+  }
+
+  getInteractionMap(sessionId: number) {
+    
+  }
 }
