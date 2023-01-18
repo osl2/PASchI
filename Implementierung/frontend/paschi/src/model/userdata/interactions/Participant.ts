dd0f199eca00768c8786a72c66f05871b10043c1
uekai <luka.kosak@student.kit.edu>
2023-01-18 13:41:18 +0100

    Implemented Participant.ts

--- Implementierung/frontend/paschi/src/model/userdata/interactions/Participant.ts
+++ Implementierung/frontend/paschi/src/model/userdata/interactions/Participant.ts
@@ -1,8 +1,46 @@
+import {User} from "@/model/User";
+import {Course} from "@/model/userdata/courses/Course";
+import {Interaction} from "@/model/userdata/interactions/Interaction";
+
 export abstract class Participant {
 
   id: number;
+  user: User;
+  firstName: string;
+  lastName: string;
+  courses: Course[];
+  interactions: Interaction[];
 
-  constructor(id: number) {
+  constructor(id: number, user: User, firstName: string, lastName: string) {
     this.id = id;
+    this.user = user;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.courses = [];
+    this.interactions = [];
+  }
+
+  addCourse(course: Course): void {
+    this.courses.push(course);
+  }
+
+  removeCourse(courseId: number): void {
+    this.courses.forEach((element, index) => {
+      if (element.id == courseId) {
+        this.courses.splice(index, 1);
+      }
+    });
+  }
+
+  addInteraction(interaction: Interaction): void {
+    this.interactions.push(interaction);
+  }
+
+  removeInteraction(interactionId: number): void {
+    this.interactions.forEach((element, index) => {
+      if (element.id == interactionId) {
+        this.interactions.splice(index, 1);
+      }
+    });
   }
 }
