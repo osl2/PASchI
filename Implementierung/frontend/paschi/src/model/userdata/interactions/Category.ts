9a2c69fbc333fd8d03f3475ac945e85bf093be60
uekai <luka.kosak@student.kit.edu>
2023-01-18 13:46:08 +0100

    Implemented Category.ts RatedCategory.ts

--- Implementierung/frontend/paschi/src/model/userdata/interactions/Category.ts
+++ Implementierung/frontend/paschi/src/model/userdata/interactions/Category.ts
@@ -1,3 +1,12 @@
 export class Category {
-  
+
+  id: number;
+  user: number;
+  name: string;
+
+  constructor(id: number, user: number, name: string) {
+    this.id = id;
+    this.user = user;
+    this.name = name;
+  }
 }
--- Implementierung/frontend/paschi/src/model/userdata/interactions/RatedCategory.ts
+++ Implementierung/frontend/paschi/src/model/userdata/interactions/RatedCategory.ts
@@ -1,5 +1,12 @@
 import {Category} from "@/model/userdata/interactions/Category";
+import {Quality} from "@/model/userdata/interactions/Quality";
 
 export class RatedCategory extends Category {
 
+  quality: Quality;
+
+  constructor(id: number, user: number, name: string, quality: Quality) {
+    super(id, user, name);
+    this.quality = quality;
+  }
 }
