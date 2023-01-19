e87a275c4c9cdc0902d0eef2f3f41c3959b244e0
Florian Knechtel <fj.knechtel@gmail.com>
2023-01-19 23:17:49 +0100

    Added Request Annotations

--- Implementierung/Backend/src/main/java/edu/kit/informatik/controller/UserController.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,7 +38,7 @@ public class UserController extends BaseController<User, UserDto> {
 
     @Override
     @PostMapping
-    public UserDto add(UserDto userDto) {
+    public UserDto add(@RequestBody UserDto userDto) {
         return super.add(userDto);
     }
 
