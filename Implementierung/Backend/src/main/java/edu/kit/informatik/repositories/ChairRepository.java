f7765a543cc76c0fcc2a12b0bdd3352a02e91778
Florian Knechtel <fj.knechtel@gmail.com>
2023-01-19 23:08:24 +0100

    Make them Own Repository (Removed Inheritance)

--- Implementierung/Backend/src/main/java/edu/kit/informatik/repositories/ChairRepository.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/repositories/ChairRepository.java
@@ -1,7 +1,11 @@
 package edu.kit.informatik.repositories;
 
+import edu.kit.informatik.dto.userdata.rooms.ChairDto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import edu.kit.informatik.model.userdata.rooms.Chair;
+
+import java.util.Optional;
 
 /**
  * Schnittstelle zur Datenbanktabelle mit {@link Chair Tabellen}
@@ -10,5 +14,7 @@ import edu.kit.informatik.model.userdata.rooms.Chair;
  * @version 1.0
  */
 @Repository
-public interface ChairRepository extends RoomObjectRepository {
+public interface ChairRepository extends JpaRepository<Chair, ChairDto> {
+
+    Optional<Chair> findChairById(String id);
 }
--- Implementierung/Backend/src/main/java/edu/kit/informatik/repositories/TableRepository.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/repositories/TableRepository.java
@@ -1,7 +1,11 @@
 package edu.kit.informatik.repositories;
 
+import edu.kit.informatik.dto.userdata.rooms.TableDto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import edu.kit.informatik.model.userdata.rooms.Table;
+
+import java.util.Optional;
 
 /**
  * Schnittstelle zur Datenbanktabelle mit {@link Table Tischen}
@@ -10,5 +14,7 @@ import edu.kit.informatik.model.userdata.rooms.Table;
  * @version 1.0
  */
 @Repository
-public interface TableRepository extends RoomObjectRepository {
+public interface TableRepository extends JpaRepository<Table, TableDto> {
+
+    Optional<Table> findTableById(String id);
 }
