20675e48a946c67775f7dc4a7426f96760d4ca04
uekai <luka.kosak@student.kit.edu>
2023-01-20 14:52:40 +0100

    Implemented CategoryMapper

--- Implementierung/Backend/src/main/java/edu/kit/informatik/dto/mapper/interactions/CategoryMapper.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/dto/mapper/interactions/CategoryMapper.java
@@ -2,22 +2,26 @@ package edu.kit.informatik.dto.mapper.interactions;
 
 import edu.kit.informatik.dto.mapper.IModelDtoMapper;
 import edu.kit.informatik.dto.userdata.interactions.CategoryDto;
+import edu.kit.informatik.model.User;
 import edu.kit.informatik.model.userdata.interactions.Category;
 import edu.kit.informatik.repositories.CategoryBaseRepository;
+import edu.kit.informatik.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CategoryMapper implements IModelDtoMapper<Category, CategoryDto> {
 
     private final CategoryBaseRepository categoryRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public CategoryMapper(CategoryBaseRepository categoryRepository) {
+    public CategoryMapper(CategoryBaseRepository categoryRepository, UserRepository userRepository) {
         this.categoryRepository = categoryRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -27,7 +31,7 @@ public class CategoryMapper implements IModelDtoMapper<Category, CategoryDto> {
 
     @Override
     public List<CategoryDto> modelToDto(List<Category> categories) {
-        List<CategoryDto> categoryDtos = new LinkedList<>();
+        List<CategoryDto> categoryDtos = new ArrayList<>();
         categories.forEach(category -> categoryDtos.add(modelToDto(category)));
 
         return categoryDtos;
@@ -35,13 +39,18 @@ public class CategoryMapper implements IModelDtoMapper<Category, CategoryDto> {
 
     @Override
     public Category dtoToModel(CategoryDto categoryDto) {
-        // repository fehlt noch
-        return null;
+        Category category = categoryRepository.findCategoryById(categoryDto.getId()).orElseGet(Category::new);
+        User user = userRepository.findUserById(categoryDto.getUserId()).orElse(null);
+
+        category.setUser(user);
+        category.setName(categoryDto.getName());
+
+        return category;
     }
 
     @Override
     public List<Category> dtoToModel(List<CategoryDto> categoryDtos) {
-        List<Category> categories = new LinkedList<>();
+        List<Category> categories = new ArrayList<>();
         categoryDtos.forEach(categoryDto -> categories.add(dtoToModel(categoryDto)));
 
         return categories;
