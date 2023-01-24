f8a1d93f923842d293db87939ed0cd76483a263b
David <uopmx@student.kit.edu>
2023-01-24 13:22:26 +0100

    viewStudentsPage implementiert

--- Implementierung/frontend/paschi/src/components/studentManagement/ViewStudentsPage.vue
+++ Implementierung/frontend/paschi/src/components/studentManagement/ViewStudentsPage.vue
@@ -1,11 +1,72 @@
 <template>
+  <navigation-bar extended>
+    <template v-slot:extension>
+      <v-btn @click="newStudentClick">Schüler erstellen</v-btn>
+    </template>
+  </navigation-bar>
 
+  <v-main>
+    <SideMenu />
+    <v-list>
+      <v-list-item>
+        David M
+        <v-btn @click="deleteStudentClick(student)">schüler löschen</v-btn>
+        <v-btn @click="editStudentClick(student)">schüler bearbeiten</v-btn>
+        <v-btn @click="showStatisticsClick(student)">Statistiken anzeigen</v-btn>
+      </v-list-item>
+      <v-list-item>
+        Bob b
+        <v-btn @click="deleteStudentClick(student)">schüler löschen</v-btn>
+        <v-btn @click="editStudentClick(student)">schüler bearbeiten</v-btn>
+        <v-btn @click="showStatisticsClick(student)">Statistiken anzeigen</v-btn>
+      </v-list-item>
+      <v-list-item v-for="student in students">
+        {{student.firstName}} {{student.firstName}}
+        <v-btn @click="deleteStudentClick(student)">schüler löschen</v-btn>
+        <v-btn @click="editStudentClick(student)">schüler bearbeiten</v-btn>
+        <v-btn @click="showStatisticsClick(student)">Statistiken anzeigen</v-btn>
+      </v-list-item>
+    </v-list>
+  </v-main>
 </template>
 
-<script>
-export default {
-  name: "viewStudentsPage"
-}
+<script lang="ts">
+import {StudentController} from "@/controller/StudentController";
+import NavigationBar from "@/components/navigation/NavigationBar.vue";
+import SideMenu from "@/components/navigation/SideMenu.vue";
+import {Student} from "@/model/userdata/interactions/Student";
+import {ref} from "vue";
+
+export default({
+  name: "viewStudentsPage",
+  components: {SideMenu, NavigationBar},
+
+  setup() {
+    const studentController = StudentController.getStudentConroller();
+    const students = ref<Student[]>(studentController.getAllStudents());
+    function newStudentClick() {
+
+    }
+    function editStudentClick(student: Student) {
+
+    }
+
+    function deleteStudentClick(student: Student) {
+
+    }
+
+    function showStatisticsClick(student: Student) {
+
+    }
+    return {
+      newStudentClick,
+      editStudentClick,
+      deleteStudentClick,
+      showStatisticsClick,
+      students
+    }
+  }
+})
 </script>
 
 <style scoped>
--- Implementierung/frontend/paschi/src/plugins/router.ts
+++ Implementierung/frontend/paschi/src/plugins/router.ts
@@ -5,6 +5,7 @@ import EditStudentPage from "@/components/studentManagement/EditStudentPage.vue"
 import Register from "@/components/authentication/Register.vue";
 import AdminPage from "@/components/authentication/AdminPage.vue";
 import RoomEditor from "@/components/interactionMaps/RoomEditor.vue";
+import ViewStudentsPage from "@/components/studentManagement/ViewStudentsPage.vue";
 
 const routes = [
   {path: '/login', name: 'Login', component: Login},
@@ -12,6 +13,7 @@ const routes = [
   {path: '/admin', name: 'AdminPage', component: AdminPage},
   {path: '/dashboard', name: 'Dashboard', component: Dashboard},
   {path: '/edit-student', name: 'EditStudentPage', component: EditStudentPage, props: { studentId: 1}},
+  {path: '/viewStudents', name: 'viewStudentsPage', component: ViewStudentsPage},
   {path: '/room-editor', name: 'InteractionMap', component: RoomEditor}
 
 ]
