77155f327c96b4bf2eb262a5a24b67d24fc8a6b0
Florian Knechtel <fj.knechtel@gmail.com>
2023-01-25 18:12:40 +0100

    Implemented CourseService

--- Implementierung/Backend/src/main/java/edu/kit/informatik/service/CourseService.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/service/CourseService.java
@@ -1,14 +1,14 @@
 package edu.kit.informatik.service;
 
-import edu.kit.informatik.dto.mapper.IModelDtoMapper;
 import edu.kit.informatik.dto.mapper.courses.CourseMapper;
 import edu.kit.informatik.dto.userdata.courses.CourseDto;
 import edu.kit.informatik.model.userdata.courses.Course;
 import edu.kit.informatik.repositories.CourseRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Service für {@link Course Kurse}
@@ -34,26 +34,52 @@ public class CourseService extends BaseService<Course, CourseDto> {
 
     @Override
     public CourseDto add(CourseDto courseDto) {
-        return null;
+        Course newCourse = this.courseRepository.save(this.mapper.dtoToModel(courseDto));
+        return this.mapper.modelToDto(newCourse);
     }
 
+    @Transactional
     @Override
     public CourseDto update(CourseDto courseDto) {
-        return null;
+        Optional<Course> repositoryCourseOptional = this.courseRepository.findById(courseDto.getId());
+
+        if (repositoryCourseOptional.isEmpty()) {
+            return null;
+        }
+
+        Course repositoryCourse = repositoryCourseOptional.get();
+        Course newCourse = this.mapper.dtoToModel(courseDto);
+
+        if (!newCourse.getName().equals(repositoryCourse.getName())) {
+            repositoryCourse.setName(newCourse.getName());
+        } else if (!newCourse.getSubject().equals(repositoryCourse.getSubject())) {
+            repositoryCourse.setSubject(newCourse.getSubject());
+        } else if (!newCourse.getParticipants().equals(repositoryCourse.getParticipants())) {
+            repositoryCourse.setParticipants(newCourse.getParticipants());
+        } else if (!newCourse.getSessions().equals(repositoryCourse.getSessions())) {
+            repositoryCourse.setSessions(newCourse.getSessions());
+        } else if (!newCourse.getSeatArrangements().equals(repositoryCourse.getSeatArrangements())) {
+            repositoryCourse.setSeatArrangements(newCourse.getSeatArrangements());
+        }
+
+        return courseDto;
     }
 
     @Override
     public CourseDto getById(String id) {
-        return null;
+        Optional<Course> courseOptional = this.courseRepository.findCourseById(id);
+
+        return courseOptional.map(this.mapper::modelToDto).orElse(null);
     }
 
     @Override
     public List<CourseDto> getAll() {
-        return null;
+        return this.mapper.modelToDto(this.courseRepository.findAll());
     }
 
     @Override
     public String delete(String id) {
-        return null;
+        this.courseRepository.deleteById(id);
+        return id;
     }
 }
