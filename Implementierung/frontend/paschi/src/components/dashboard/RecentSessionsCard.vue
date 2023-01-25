af7fdcf35f6b2b03ba01d5db59cf5de37a696146
uezbf <uezbf@student.kit.edu>
2023-01-25 18:07:27 +0100

    Improve design of RecentSessionsCard

--- Implementierung/frontend/paschi/src/components/dashboard/RecentSessionsCard.vue
+++ Implementierung/frontend/paschi/src/components/dashboard/RecentSessionsCard.vue
@@ -1,36 +1,45 @@
 <template>
   <v-card color="primary">
     <v-card-title>
-      <v-row class="ma-2">
-        Letzte Sitzungen
-      </v-row>
+      <v-row class="ma-2"> Letzte Sitzungen </v-row>
     </v-card-title>
     <v-card-item v-for="session in sessions">
-      {{session.name}}
-      <v-btn @click="navigateTo(session)"></v-btn>
+      <v-row class="ma-2">
+        {{ session.name }}
+        <v-spacer />
+        <v-btn @click="navigateTo(session)"></v-btn>
+      </v-row>
     </v-card-item>
   </v-card>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
-import {SessionController} from "@/controller/SessionController";
+import { defineComponent } from "vue";
+import { SessionController } from "@/controller/SessionController";
 
 export default defineComponent({
   name: "RecentSessionsCard",
   setup() {
+    const sessionController = SessionController.getSessionController();
 
-    const sessionController = SessionController.getSessionController()
-
-    const sessions = sessionController.getAllSessions()
+    //const sessions = sessionController.getAllSessions()
+    const sessions = [
+      {
+        name: "Test",
+      },
+      {
+        name: "Test2",
+      },
+      {
+        name: "Test3",
+      },
+    ];
 
     return {
-      sessions
-    }
-  }
-})
+      sessions,
+    };
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
