9e5ee10f46c62187ddf1289a70b125eb743c5eca
Florian Knechtel <fj.knechtel@gmail.com>
2023-01-25 15:30:57 +0100

    Implemented ParticipantService

--- Implementierung/Backend/src/main/java/edu/kit/informatik/service/ParticipantService.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/service/ParticipantService.java
@@ -1,14 +1,14 @@
 package edu.kit.informatik.service;
 
-import edu.kit.informatik.dto.mapper.IModelDtoMapper;
 import edu.kit.informatik.dto.mapper.interactions.ParticipantMapper;
 import edu.kit.informatik.dto.userdata.interactions.ParticipantDto;
 import edu.kit.informatik.model.userdata.interactions.Participant;
 import edu.kit.informatik.repositories.ParticipantRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Service für {@link Participant Teilnehmer}
@@ -35,26 +35,53 @@ public class ParticipantService extends BaseService<Participant, ParticipantDto>
 
     @Override
     public ParticipantDto add(ParticipantDto participantDto) {
-        return null;
+        Participant newParticipant = this.participantRepository.save(this.mapper.dtoToModel(participantDto));
+
+        return this.mapper.modelToDto(newParticipant);
     }
 
+    @Transactional
     @Override
     public ParticipantDto update(ParticipantDto participantDto) {
-        return null;
+        Optional<Participant> repositoryParticipantOptional = this.participantRepository
+                                                                    .findParticipantById(participantDto.getId());
+
+        if (repositoryParticipantOptional.isEmpty()) {
+            return null;
+        }
+
+        Participant repositoryParticipant = repositoryParticipantOptional.get();
+        Participant newParticipant = this.mapper.dtoToModel(participantDto);
+
+        if (!newParticipant.getFirstName().equals(repositoryParticipant.getFirstName())) {
+            repositoryParticipant.setFirstName(newParticipant.getFirstName());
+        } else if (!newParticipant.getLastName().equals(repositoryParticipant.getLastName())) {
+            repositoryParticipant.setLastName(newParticipant.getLastName());
+        } else if (!newParticipant.getCourses().equals(repositoryParticipant.getCourses())) {
+            repositoryParticipant.setCourses(newParticipant.getCourses());
+        } else if (!newParticipant.getInteractions().equals(repositoryParticipant.getInteractions())) {
+            repositoryParticipant.setInteractions(newParticipant.getInteractions());
+        }
+
+        return participantDto;
     }
 
     @Override
     public ParticipantDto getById(String id) {
-        return null;
+        Optional<Participant> participantOptional = this.participantRepository.findParticipantById(id);
+
+        return participantOptional.map(this.mapper::modelToDto).orElse(null);
     }
 
     @Override
     public List<ParticipantDto> getAll() {
-        return null;
+        return this.mapper.modelToDto(this.participantRepository.findAll());
     }
 
     @Override
     public String delete(String id) {
-        return null;
+        this.participantRepository.deleteById(id);
+
+        return id;
     }
 }
