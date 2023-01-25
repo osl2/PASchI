8b916b169d34c00c673d863417e06c45676e3ac2
uekai <luka.kosak@student.kit.edu>
2023-01-25 18:59:43 +0100

    Implemented RoomController.ts

--- Implementierung/frontend/paschi/src/controller/RoomController.ts
+++ Implementierung/frontend/paschi/src/controller/RoomController.ts
@@ -1,9 +1,16 @@
 import {Room} from "@/model/userdata/rooms/Room";
 import {Position} from "@/model/userdata/rooms/Position";
+import {useRoomStore} from "@/store/RoomStore";
+import {UserController} from "@/controller/UserController";
+import {Chair} from "@/model/userdata/rooms/Chair";
+import {useRoomObjectStore} from "@/store/RoomObjectStore";
+import {Table} from "@/model/userdata/rooms/Table";
 
 export class RoomController {
 
   private static controller: RoomController = new RoomController();
+  private roomStore = useRoomStore();
+  private userController = UserController.getUserController();
 
   private constructor() {
   }
@@ -13,34 +20,64 @@ export class RoomController {
   }
 
   createRoom(name: string): string {
-    return "";
+    let room = new Room(undefined, this.roomStore.getNextId(), this.userController.getUser(), name);
+    this.roomStore.addRoom(room);
+
+    return room.getId;
   }
 
-  updateRoom(id: string) {
-
+  updateRoom(id: string, name: string) {
+    let room = this.roomStore.getRoom(id);
+    if (room !== undefined) {
+      room.name = name;
+    }
   }
 
   deleteRoom(id: string) {
-
+    this.roomStore.deleteRoom(id);
   }
 
   getRoom(id: string): Room | undefined {
-    return undefined;
+    let room = this.roomStore.getRoom(id);
+    if (room == undefined) {
+      return undefined;
+    }
+    return room;
   }
 
   getAllRooms(): Room[] {
-    return [];
+    return this.roomStore.getAllRooms();
   }
 
-  addChair(roomId: string, pos: Position): string {
-    return "";
+  addChair(roomId: string, pos: Position): string | undefined {
+    let room = this.roomStore.getRoom(roomId);
+    if (room == undefined) {
+      return undefined;
+    }
+
+    let chair = new Chair(undefined, useRoomObjectStore().getNextId(), this.userController.getUser(), pos);
+    room.addRoomObject(chair);
+
+    return chair.getId;
   }
 
-  addTable(roomId: string, pos: Position, length: number, width: number): string {
-    return "";
+  addTable(roomId: string, pos: Position, length: number, width: number): string | undefined {
+    let room = this.roomStore.getRoom(roomId);
+    if (room == undefined) {
+      return undefined;
+    }
+
+    let table = new Table(undefined, useRoomObjectStore().getNextId(), this.userController.getUser(), pos, length,
+      width);
+    room.addRoomObject(table);
+
+    return table.getId;
   }
 
   removeRoomObject(roomId: string, objectId: string) {
-
+    let room = this.roomStore.getRoom(roomId);
+    if (room !== undefined) {
+      room.removeRoomObject(objectId);
+    }
   }
 }
