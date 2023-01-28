9efeeac0a687672a6db0f61408d32f6021653c35
Cián Payne <ciandanielpayne@gmail.com>
2023-01-28 17:31:57 +0100

    Improve register page and remove redundant scroll bar

--- Implementierung/frontend/paschi/src/components/userManagement/Register.vue
+++ Implementierung/frontend/paschi/src/components/userManagement/Register.vue
@@ -1,49 +1,89 @@
 <template>
-  <v-main>
-    <v-container fluid class="h-100">
-      <v-row  class="h-100" align="center" justify="center">
-        <v-card class="v-col-11 bg-grey-lighten-3 elevation-6" max-width="500px">
-          <v-card-title class="text-center" >
-            Bitte registrieren Sie sich.
-          </v-card-title>
-          <v-card-item>
-            <v-text-field class="mt-2" variant="outlined" label="Vorname" />
-            <v-text-field class="mt-2" variant="outlined" label="Nachname" />
-            <v-text-field class="mt-2" variant="outlined" label="Mailadresse" />
-            <v-text-field type="password" variant="outlined" label="Passwort" />
-            <v-text-field type="password" variant="outlined" label="Passwort bestätigen" />
-          </v-card-item>
-          <v-card-item>
-            <v-btn block prepend-icon="fas fa-sign-in-alt" color="primary" >Registrieren</v-btn>
-          </v-card-item>
-            <v-card-item>
-            <v-btn block color="primary" @click="router.push('Login')" >Schon registriert? Zum Login</v-btn>
-          </v-card-item>
-        </v-card>
-      </v-row>
+  <v-main
+    class="ma-0 bg-grey-lighten-3 v-row justify-center align-content-xl-space-around"
+    :style="{
+      borderTop: 'solid',
+      borderWidth: '5px',
+      borderTopColor: $vuetify.theme.current.colors.primary,
+    }"
+  >
+    <v-container fluid class="v-col-11" style="max-width: 450px">
+      <v-card class="pa-2" variant="text">
+        <v-card-title
+          class="text-h4 text-center text-indigo-darken-4 text-wrap"
+        >
+          Gleich geschafft!
+        </v-card-title>
+        <v-card-subtitle
+          class="text-xxl-caption text-center text-indigo-darken-3 text-wrap"
+        >
+          Registrieren Sie sich jetzt und nutzen Sie PAschI im vollen Umfang.
+        </v-card-subtitle>
+      </v-card>
+      <v-card class="pa-2 rounded-lg" variant="flat" max-width="450px">
+        <v-card-item>
+          <v-text-field class="mt-2" variant="outlined" label="Vorname" />
+          <v-text-field variant="outlined" label="Nachname" />
+          <v-text-field
+            prepend-inner-icon="mdi mdi-email-outline"
+            variant="outlined"
+            label="Mailadresse"
+          />
+          <v-text-field
+            prepend-inner-icon="mdi mdi-lock-outline"
+            type="password"
+            variant="outlined"
+            label="Passwort"
+          />
+          <v-text-field
+            prepend-inner-icon="mdi mdi-lock-outline"
+            type="password"
+            variant="outlined"
+            label="Passwort bestätigen"
+          />
+        </v-card-item>
+        <v-card-item>
+          <v-btn
+            block
+            height="50"
+            variant="flat"
+            prepend-icon="fas fa-sign-in-alt"
+            color="primary"
+            >Registrieren</v-btn
+          >
+        </v-card-item>
+      </v-card>
+      <v-card
+        class="mt-8 pa-2 rounded-lg bg-grey-lighten-2"
+        variant="flat"
+        @click="router.push('Login')"
+      >
+        <v-card-title
+          class="text-sm-button align-content-center text-center text-grey-darken-3 text-wrap"
+        >
+          Schon registriert? &middot; Zum Login
+        </v-card-title>
+      </v-card>
     </v-container>
   </v-main>
 </template>
 
 <script lang="ts">
-
-import {useUserStore} from "@/store/UserStore";
+import { useUserStore } from "@/store/UserStore";
 import AppBar from "@/components/navigation/NavigationBar.vue";
 import router from "@/plugins/router";
 export default {
   name: "Register",
-  components: {AppBar},
+  components: { AppBar },
   setup() {
-    const userStore = useUserStore()
+    const userStore = useUserStore();
 
     return {
       userStore,
-      router
-    }
-  }
-}
+      router,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
