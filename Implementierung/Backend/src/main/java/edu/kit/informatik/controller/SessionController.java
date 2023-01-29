bb296cd355554197c2777e31b893e37b2ce0babe
Florian Knechtel <fj.knechtel@gmail.com>
2023-01-29 22:31:00 +0100

    Added Parameter Annotations

--- Implementierung/Backend/src/main/java/edu/kit/informatik/controller/SessionController.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/controller/SessionController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -34,13 +36,13 @@ public class SessionController extends BaseController<Session, SessionDto, Sessi
 
     @Override
     @PostMapping
-    public SessionDto add(SessionDto sessionDto) {
+    public SessionDto add(@RequestBody SessionDto sessionDto) {
         return super.add(sessionDto);
     }
 
     @Override
     @PutMapping
-    public SessionDto update(SessionDto sessionDto) {
+    public SessionDto update(@RequestBody SessionDto sessionDto) {
         return super.update(sessionDto);
     }
 
@@ -58,7 +60,7 @@ public class SessionController extends BaseController<Session, SessionDto, Sessi
 
     @Override
     @DeleteMapping
-    public String delete(String id) {
+    public String delete(@RequestParam String id) {
         return super.delete(id);
     }
 }
