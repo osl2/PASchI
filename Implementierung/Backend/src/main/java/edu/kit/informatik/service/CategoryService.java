6b8a8d3cb6bf62a3294ea232c97c867992312474
Florian Knechtel <fj.knechtel@gmail.com>
2023-01-29 15:01:59 +0100

    Implemented CategoryService

--- Implementierung/Backend/src/main/java/edu/kit/informatik/service/CategoryService.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/service/CategoryService.java
@@ -5,12 +5,15 @@ import edu.kit.informatik.dto.mapper.interactions.CategoryMapper;
 import edu.kit.informatik.dto.userdata.interactions.CategoryDto;
 import edu.kit.informatik.dto.userdata.interactions.RatedCategoryDto;
 import edu.kit.informatik.model.userdata.interactions.Category;
+import edu.kit.informatik.model.userdata.interactions.RatedCategory;
 import edu.kit.informatik.repositories.CategoryBaseRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Service für {@link Category Kategorien}
@@ -38,28 +41,53 @@ public class CategoryService extends BaseService<Category, RatedCategoryDto, Cat
 
     @Override
     public CategoryDto add(@RequestBody RatedCategoryDto categoryDto) {
-        System.out.println(categoryDto.getId());
-        Category newCategory = (Category) this.categoryBaseRepository.save(this.mapper.dtoToModel(categoryDto));
+        //System.out.println(categoryDto.getId());
+        Category newCategory = this.categoryBaseRepository.save(this.mapper.dtoToModel(categoryDto));
+        //this.mapper.dtoToModel(categoryDto);
+        //this.categoryBaseRepository.save(new Category());
         return this.mapper.modelToDto(newCategory);
     }
 
+    @Transactional
     @Override
     public CategoryDto update(RatedCategoryDto categoryDto) {
-        return null;
+        Optional<Category> repositoryCategoryOptional = this.categoryBaseRepository
+                                                                .findCategoryById(categoryDto.getId());
+
+        if (repositoryCategoryOptional.isEmpty()) {
+            return null;
+        }
+
+        Category repositoryCategory = repositoryCategoryOptional.get();
+        Category newCategory = this.mapper.dtoToModel(categoryDto);
+
+        if (!newCategory.getName().equals(repositoryCategory.getName())) {
+            repositoryCategory.setName(newCategory.getName());
+        } else {
+            if (repositoryCategory instanceof RatedCategory ratedRepositoryCategory
+                    && newCategory instanceof  RatedCategory ratedNewCategory) {
+                ratedRepositoryCategory.setQuality(ratedNewCategory.getQuality());
+            }
+        }
+
+        return categoryDto;
     }
 
     @Override
     public CategoryDto getById(String id) {
-        return null;
+        Optional<Category> categoryOptional = this.categoryBaseRepository.findCategoryById(id);
+
+        return categoryOptional.map(this.mapper::modelToDto).orElse(null);
     }
 
     @Override
     public List<CategoryDto> getAll() {
-        return null;
+        return this.mapper.modelToDto(this.categoryBaseRepository.findAll());
     }
 
     @Override
     public String delete(String id) {
-        return null;
+        this.categoryBaseRepository.deleteById(id);
+        return id;
     }
 }
