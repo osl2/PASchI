ae81349a45dfc6c865fc8019762ac43f30e3d326
uekai <luka.kosak@student.kit.edu>
2023-01-31 14:37:19 +0100

    Implemented SeatArrangementService.ts

---
+++ Implementierung/frontend/paschi/src/service/SeatArrangementService.ts
@@ -0,0 +1,33 @@
+import {BaseService} from "@/service/BaseService";
+import {SeatArrangement} from "@/model/userdata/courses/SeatArrangement";
+import {SeatArrangementDto} from "@/dto/userdata/courses/SeatArrangementDto";
+
+export class SeatArrangementService extends BaseService<SeatArrangement, SeatArrangementDto> {
+
+  private readonly SEAT_ARRANGEMENT_BASE_URL: string;
+
+  constructor(base_url: string, SEAT_ARRANGEMENT_BASE_URL: string) {
+    super(base_url);
+    this.SEAT_ARRANGEMENT_BASE_URL = SEAT_ARRANGEMENT_BASE_URL;
+  }
+
+  add(e: SeatArrangement) {
+    super.add(e);
+  }
+
+  update(e: SeatArrangement) {
+    super.update(e);
+  }
+
+  getById(id: string) {
+    super.getById(id);
+  }
+
+  getAll() {
+    super.getAll();
+  }
+
+  delete(id: string) {
+    super.delete(id);
+  }
+}
