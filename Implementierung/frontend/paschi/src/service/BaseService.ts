ec645979869a99896828f1cfb5c4adb0872319c2
uekai <luka.kosak@student.kit.edu>
2023-01-31 14:15:26 +0100

    Added BaseService.ts

--- Implementierung/frontend/paschi/src/service/BaseService.ts
+++ Implementierung/frontend/paschi/src/service/BaseService.ts
@@ -1,3 +1,28 @@
 export abstract class BaseService<Entity, Dto> {
 
+  private base_url: string;
+
+  protected constructor(base_url: string) {
+    this.base_url = base_url;
+  }
+
+  add(e: Entity) {
+
+  }
+
+  update(e: Entity) {
+
+  }
+
+  getById(id: string)/*: Entity*/ {
+
+  }
+
+  getAll() {
+
+  }
+
+  delete(id: string) {
+
+  }
 }
