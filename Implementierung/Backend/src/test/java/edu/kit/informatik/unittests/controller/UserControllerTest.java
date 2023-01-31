c2a70121de08d4d9c41fe2ca2e5148f8dc5556a8
Florian Knechtel <fj.knechtel@gmail.com>
2023-01-31 17:42:38 +0100

    Create UserControllerTest extends AbstractTest-Class

--- Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/UserControllerTest.java
+++ Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/UserControllerTest.java
@@ -4,69 +4,84 @@ import edu.kit.informatik.dto.UserDto;
 import edu.kit.informatik.dto.mapper.UserMapper;
 import edu.kit.informatik.model.Role;
 import edu.kit.informatik.model.User;
-import edu.kit.informatik.service.UserService;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-//import static org.junit.jupiter.api.AssertEquals.assertEquals;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+public class UserControllerTest extends AbstractTest {
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(UserControllerTest.class)
-public class UserControllerTest {
+    private static final String BASE_URL = "/api/user";
 
     @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private UserService userService;
-
-    @MockBean
     private UserMapper userMapper;
 
+    private List<UserDto> userDtos;
+
+    @Before
+    @Override
+    public void setUp() {
+        super.setUp();
+
+    }
+    /*
+    private List<User> addSomeUsers() {
+
+    }
+
+     */
+
+
     @Test
-    public void getUserById() throws Exception {
-        //User user = getNewUser();
-        //UserDto userDto = userMapper.modelToDto(user);
+    public void getUserList() throws Exception {
+        String uri = "/api/user/admin";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
-        //given(userService.add(userDto)).willReturn(userDto);
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
 
+        List<User> expectedUsers = List.of(getNewUser());
+       // users.add(getNewUser());
 
-        RequestBuilder request = MockMvcRequestBuilders.get("/api/user/admin").contentType(MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(request)
-                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("test2.user@kit.edu"))
-                .andReturn();
-
-        assertEquals(result.getResponse().getContentAsString(), " ");
-
+        String expected = "[{\"id\":\"9e62c86f-945b-497d-93b3-d45b86be9a82\",\"firstName\":\"test2\",\"lastName\":\"user2\",\"email\":\"test2.user@kit.edu\",\"password\":\"password2\",\"auth\":false,\"token\":null,\"role\":\"USER\"}]";
 
 
-        //MockHttpServletResponse response = mockMvc.perform((RequestBuilder) post("/api/users").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+        //assertEquals(super.mapToJson(users), content);
+        List<UserDto> userDtoList = new ArrayList<UserDto>(Arrays.asList(super.mapFromJson(content, UserDto[].class)));
 
+        List<User> userList = userMapper.dtoToModel(userDtoList);
 
+        assertEquals(expectedUsers.size(), userList.size());
+        for (int i = 0; i< expectedUsers.size(); i++) {
+            //userList.get(i).setId(null);
+            assertEquals(expectedUsers.get(i).toString(), userList.get(i).toString());
+        }
+
+        //assertEquals(expectedUsers., userList);
+
+        //assertTrue(userList.length > 0);
     }
 
     private User getNewUser() {
         User user = new User();
+
         user.setRole(Role.USER);
 
-        user.setEmail("testuser@test.com");
-        user.setFirstName("Test");
-        user.setLastName("User");
-        user.setPassword("passwort");
+        user.setEmail("test2.user@kit.edu");
+        user.setFirstName("test2");
+        user.setLastName("user2");
+        user.setPassword("password2");
 
         return user;
     }
