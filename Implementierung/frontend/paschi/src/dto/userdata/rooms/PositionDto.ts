54b411a8a6e9c27db923d7c738559e2b528d1f34
uekai <luka.kosak@student.kit.edu>
2023-01-31 13:33:44 +0100

    Implemented PositionDto.ts

--- Implementierung/frontend/paschi/src/dto/userdata/rooms/PositionDto.ts
+++ Implementierung/frontend/paschi/src/dto/userdata/rooms/PositionDto.ts
@@ -1,3 +1,16 @@
 export class PositionDto {
 
+  id: string;
+  userId: string;
+  xCoordinate: number;
+  yCoordinate: number;
+  orientation: number;
+
+  constructor(id: string, userId: string, xCoordinate: number, yCoordinate: number, orientation: number) {
+    this.id = id;
+    this.userId = userId;
+    this.xCoordinate = xCoordinate;
+    this.yCoordinate = yCoordinate;
+    this.orientation = orientation;
+  }
 }
