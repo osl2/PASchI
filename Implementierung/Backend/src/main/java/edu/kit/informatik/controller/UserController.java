fafdd0a181f9281ba5b696762eaf1ebcb6c57e98
Florian Knechtel <fj.knechtel@gmail.com>
2023-01-31 20:01:02 +0100

    Added @RequestParam Annotation

--- Implementierung/Backend/src/main/java/edu/kit/informatik/controller/UserController.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -62,7 +63,7 @@ public class UserController extends BaseController<User, UserDto, UserDto> {
 
     @Override
     @DeleteMapping
-    public String delete(String id) {
+    public String delete(@RequestParam String id) {
         return super.delete(id);
     }
 
