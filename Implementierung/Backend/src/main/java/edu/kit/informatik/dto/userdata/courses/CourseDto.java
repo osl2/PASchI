87a25f10d3e3294136c3da03b8f632e8a2eee59b
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-01 19:49:30 +0100

    Implemented Comparable, Added @ToString and @EqualsAndHashCode Annotation

--- Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/courses/CourseDto.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/courses/CourseDto.java
@@ -2,11 +2,14 @@ package edu.kit.informatik.dto.userdata.courses;
 
 import edu.kit.informatik.model.userdata.courses.Course;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Data-Transfer-Object zur Klasse {@link Course}
@@ -14,12 +17,15 @@ import java.util.List;
  * @author uekai
  * @author ugqbo
  * @version 1.0
+
  */
+@ToString
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
-public class CourseDto {
+public class CourseDto implements Comparable<CourseDto> {
 
     private String id;
     private String userId;
@@ -28,4 +34,12 @@ public class CourseDto {
     private List<String> sessionIds;
     private List<String> participantIds;
     private List<String> seatArrangementIds;
+
+    @Override
+    public int compareTo(CourseDto o) {
+        UUID thisUser = UUID.fromString(this.id);
+        UUID oUser = UUID.fromString(o.getId());
+
+        return  thisUser.compareTo(oUser);
+    }
 }
