0efa564ee62328789519fde235968198c9744aed
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-01 19:27:29 +0100

    Implemented AddCourseToDataBase

--- Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/CourseControllerTest.java
+++ Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/CourseControllerTest.java
@@ -1,16 +1,25 @@
 package edu.kit.informatik.unittests.controller;
 
+
+import com.github.javafaker.Faker;
 import edu.kit.informatik.dto.mapper.courses.CourseMapper;
 import edu.kit.informatik.dto.userdata.courses.CourseDto;
+import edu.kit.informatik.repositories.CourseRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class CourseControllerTest extends AbstractTest {
 
@@ -19,11 +28,77 @@ public class CourseControllerTest extends AbstractTest {
     @Autowired
     private CourseMapper courseMapper;
 
-    private List<CourseDto> courseDtos;
+    @Autowired
+    private CourseRepository courseRepository;
+
+    private List<CourseDto> courses;
+
+    @Before
+    @Override
+    public void setUp() {
+        super.setUp();
+        this.courses = addSomeCourses();
+    }
+
+    private List<CourseDto> addSomeCourses() {
+
+        List<CourseDto> courseDtos = new ArrayList<>();
+        Faker faker = new Faker(new Locale("de"));
+
+        for (int i = 0; i < 1; i++) {
+            courseDtos.add(getNewCourse(faker));
+        }
+
+        return courseDtos;
+
+    }
+
+    private void deleteFromDataBase() {
+
+        for (CourseDto courseDto: courses) {
+            if (courseDto.getId() != null) {
+                this.courseRepository.deleteById(courseDto.getId());
+            }
+        }
+
+        courses.clear();
+    }
 
 
     @Test
-    public void getAllCourses() throws Exception{
+    public void addCourseToDatabase() throws Exception {
+        for (CourseDto courseDto: courses) {
+            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(BASE_URL).contentType(MediaType.APPLICATION_JSON)
+                            .content(super.mapToJson(courseDto))
+                    //.accept(MediaType.APPLICATION_JSON_VALUE)
+            ).andReturn();
 
+            int status = mvcResult.getResponse().getStatus();
+            assertEquals(200, status);
+            String content = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+            CourseDto courseDtoFromContent = super.mapFromJson(content, CourseDto.class);
+
+            assertEquals(courseDto.getName(), courseDtoFromContent.getName());
+            assertEquals(courseDto.getSubject(), courseDtoFromContent.getSubject());
+            assertNotNull(courseDtoFromContent.getId());
+            courses.set(courses.indexOf(courseDto), courseDtoFromContent);
+        }
+        deleteFromDataBase();
+    }
+
+
+
+    private CourseDto getNewCourse(Faker faker) {
+
+        CourseDto courseDto = new CourseDto();
+
+        String name = faker.team().name();
+
+        courseDto.setName(name + " " + faker.number().randomDigit());
+        courseDto.setSubject(name);
+        courseDto.setUserId("6d421cec-d640-414e-a499-c62ead382b04");
+
+        return courseDto;
     }
 }
