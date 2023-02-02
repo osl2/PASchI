fa172d6976215e81a94d482e77993d06bb4b757c
David <uopmx@student.kit.edu>
2023-02-02 15:51:04 +0100

    upgraded requestListCard

--- Implementierung/frontend/paschi/src/components/userManagement/RequestListCard.vue
+++ Implementierung/frontend/paschi/src/components/userManagement/RequestListCard.vue
@@ -1,57 +1,166 @@
 <template>
-  <v-card color="primary">
+  <v-card color="primary" min-width="600">
     <v-card-title>
       <v-row class="ma-2">
-       Accountanträge
+        Accountanträge
+        <v-spacer />
+        <v-btn @click="toggleCollapse">{{ toggleCollapseMessage }}</v-btn>
       </v-row>
     </v-card-title>
-
-    <v-card-item v-for="request in requests">
-      <v-row class="ma-2">
-        {{ request.name }}
-        <v-spacer />
-        <v-btn prepend-icon="fas fa-check" color="primary" @click="authUser(request)" />
-        <v-btn prepend-icon="fas fa-xmark" color="primary" @click="deleteUser(request)" />
+    <v-card-item v-show="!collapsed">
+      <v-select
+        label="Anzeigeparameter"
+        :items="displayParameters"
+        v-model="displayParameter"
+      />
+    </v-card-item>
+    <v-card-item v-show="!collapsed">
+      <v-select
+        label="suchen nach"
+        :items="searchParameters"
+        v-model="searchParameter"
+      />
+      <v-text-field
+        v-model="searchInput"
+        label="Suche"
+        type="input"
+      ></v-text-field>
+    </v-card-item>
+    <v-card-item
+      v-for="request in requests"
+      v-show="!collapsed && includesSearch(request)"
+    >
+      <v-row class="ma-2" align-content="start">
+        <v-col v-show="showName">
+          {{ request.firstName }} + {{ request.lastName }}
+        </v-col>
+        <v-col v-show="showEmail">
+          {{ request.email }}
+        </v-col>
+        <v-col>
+          {{ request.getId }}
+        </v-col>
+        <v-col cols="2">
+          <v-btn
+            prepend-icon="fas fa-check"
+            color="#00ff00"
+            @click="authUser(request)"
+          />
+          <v-btn
+            prepend-icon="fas fa-xmark"
+            color="#ff0000"
+            @click="deleteUser(request)"
+          />
+        </v-col>
       </v-row>
     </v-card-item>
   </v-card>
 </template>
 
-
-
 <script lang="ts">
-
-import {defineComponent, ref, Ref} from "vue";
+import {computed, defineComponent, ref, Ref} from "vue";
 import {AdminController} from "@/controller/AdminController";
 import {User} from "@/model/User";
+import {Role} from "@/model/Role";
+import {UserController} from "@/controller/UserController";
 
 export default defineComponent({
   name: "RequestListCard",
   setup() {
-    const adminController: AdminController = AdminController.getAdminController();
-    const requests: Ref<User[]> = ref<User[]>(adminController.getUsersNotAuthenticated()) as Ref<User[]>;
-
+      const userController: UserController = UserController.getUserController()
+      userController.register("David", "nachname1", "email1", "", "")
+      userController.register("Cian", "nachname2", "email2", "", "")
+      userController.register("Aaron", "nachname3", "email3", "", "")
+      userController.register("Luka", "nachname4", "email4", "", "")
+      userController.register("Florian", "nachname5", "email5", "", "")
+    const adminController: AdminController =
+      AdminController.getAdminController();
+    const requests: Ref<User[]> = ref<User[]>(
+      adminController.getUsersNotAuthenticated()
+    ) as Ref<User[]>;
+    const searchInput = ref("");
+    const searchParameters = computed(() => {
+      if (
+        displayParameter.value === "E-Mail + ID" ||
+        displayParameter.value === "alle"
+      ) {
+        return ["E-Mail", "ID"];
+      }
+      searchParameter.value = "ID";
+      return ["ID"];
+    });
+    const searchParameter = ref("ID");
+    const collapsed = ref(true);
+    const toggleCollapseMessage = ref("ausklappen");
+    const displayParameters: string[] = [
+      "Benutzername + ID",
+      "E-Mail + ID",
+      "alle",
+    ];
+    const displayParameter = ref("alle");
+    const showName = computed(() => {
+      return (
+        displayParameter.value === "Benutzername + ID" ||
+        displayParameter.value === "alle"
+      );
+    });
+    const showEmail = computed(() => {
+      return (
+        displayParameter.value === "E-Mail + ID" ||
+        displayParameter.value === "alle"
+      );
+    });
+    function includesSearch(request: User): boolean {
+      if (
+        searchParameter.value === "ID" &&
+        request.getId.includes(searchInput.value)
+      ) {
+        return true;
+      }
+      if (
+        searchParameter.value === "E-Mail" &&
+        request.email.includes(searchInput.value)
+      ) {
+        return true;
+      }
+      return false;
+    }
     function authUser(user: User) {
       adminController.authUser(user.getId);
     }
     function deleteUser(user: User) {
       adminController.deleteUser(user.getId);
     }
+    function toggleCollapse() {
+      collapsed.value = !collapsed.value;
+      if (collapsed.value) {
+        toggleCollapseMessage.value = "ausklappen";
+      } else {
+        toggleCollapseMessage.value = "einklappen";
+      }
+    }
     return {
+      toggleCollapse,
       authUser,
       deleteUser,
-      //request,
+      includesSearch,
+      searchParameter,
+      searchParameters,
+      showEmail,
+      showName,
+      displayParameters,
+      displayParameter,
+      collapsed,
+      toggleCollapseMessage,
+      searchInput,
       requests: [
-        { name: "Hansi"},
-        {name: "Gudrun"},
-        { name: "Hansi"}
+        new User(undefined, 0, "David", "nachname1", "email1", false, Role.USER, ""),
+        new User(undefined, 1, "Cian", "nachname2", "email1", false, Role.USER, ""),
+        new User(undefined, 2, "Luka", "nachname3", "email1", false, Role.USER, ""),
       ]
-    }
-  }
-})
-
+  };
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
