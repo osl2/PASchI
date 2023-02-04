5e1a5a19493224d1a7892ef68decf8fba95612ad
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-04 15:25:58 +0100

    Added Annotation and implemented Comparable

--- Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/courses/SessionDto.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/courses/SessionDto.java
@@ -4,12 +4,15 @@ package edu.kit.informatik.dto.userdata.courses;
 import edu.kit.informatik.dto.userdata.interactions.InteractionDto;
 import edu.kit.informatik.model.userdata.courses.Session;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import edu.kit.informatik.model.userdata.interactions.Interaction;
+import lombok.ToString;
+
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Data-Transfer-Object zur Klasse {@link Session}
@@ -18,11 +21,14 @@ import java.util.List;
  * @author ugqbo
  * @version 1.0
  */
+
+@ToString
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class SessionDto {
+public class SessionDto implements Comparable<SessionDto> {
 
     /**
      * Liste von {@link InteractionDto}
@@ -35,4 +41,12 @@ public class SessionDto {
     private String date;
     private String courseId;
     private String seatArrangementId;
+
+    @Override
+    public int compareTo(SessionDto o) {
+        UUID thisUser = UUID.fromString(this.id);
+        UUID oUser = UUID.fromString(o.getId());
+
+        return  thisUser.compareTo(oUser);
+    }
 }
