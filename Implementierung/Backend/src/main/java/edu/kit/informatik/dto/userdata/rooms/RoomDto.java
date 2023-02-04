8cd427877129145f523802309d2718c665683325
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-04 16:16:55 +0100

    Added Annotations & implemented Comparable

--- Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/rooms/RoomDto.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/rooms/RoomDto.java
@@ -2,11 +2,14 @@ package edu.kit.informatik.dto.userdata.rooms;
 
 import edu.kit.informatik.model.userdata.rooms.Room;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Data-Transfer-Object zur Klasse {@link Room}
@@ -15,14 +18,25 @@ import java.util.List;
  * @author ugqbo
  * @version 1.0
  */
+
+@ToString
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class RoomDto {
+public class RoomDto implements Comparable<RoomDto> {
 
     private String id;
     private String userId;
     private String name;
     private List<RoomObjectDto> roomObjects;
+
+    @Override
+    public int compareTo(RoomDto o) {
+        UUID thisUser = UUID.fromString(this.id);
+        UUID oUser = UUID.fromString(o.getId());
+
+        return  thisUser.compareTo(oUser);
+    }
 }
