c8f6a1f3d3cb687fb79ec3798213cabbf864a7df
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-04 21:22:43 +0100

    Added Annotations and implements Comparable

--- Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/interactions/CategoryDto.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/interactions/CategoryDto.java
@@ -2,9 +2,13 @@ package edu.kit.informatik.dto.userdata.interactions;
 
 import edu.kit.informatik.model.userdata.interactions.Category;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
 
 /**
  * Data-Transfer-Object zur Klasse {@link Category}
@@ -13,13 +17,24 @@ import lombok.Setter;
  * @author ugqbo
  * @version 1.0
  */
+
+@EqualsAndHashCode
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class CategoryDto {
+public class CategoryDto implements Comparable<CategoryDto> {
 
     private String id;
     private String userId;
     private String name;
+
+    @Override
+    public int compareTo(CategoryDto o) {
+        UUID thisUser = UUID.fromString(this.id);
+        UUID oUser = UUID.fromString(o.getId());
+
+        return  thisUser.compareTo(oUser);
+    }
 }
