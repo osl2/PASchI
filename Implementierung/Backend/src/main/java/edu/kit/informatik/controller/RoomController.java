3d595ac7c7d2a1fccba3150a30d4b1105c412853
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-04 16:24:46 +0100

    Added @RequestBody-Annotations

--- Implementierung/Backend/src/main/java/edu/kit/informatik/controller/RoomController.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/controller/RoomController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,13 +34,13 @@ public class RoomController extends BaseController<Room, RoomDto, RoomDto> {
 
     @Override
     @PostMapping
-    public RoomDto add(RoomDto roomDto) {
+    public RoomDto add(@RequestBody RoomDto roomDto) {
         return super.add(roomDto);
     }
 
     @Override
     @PutMapping
-    public RoomDto update(RoomDto roomDto) {
+    public RoomDto update(@RequestBody RoomDto roomDto) {
         return super.update(roomDto);
     }
 
