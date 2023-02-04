86c22c884375fa9e6f89b76601b3d3314b893b75
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-04 19:51:26 +0100

    Added Annotations and implemented Comparable

--- Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/courses/SeatArrangementDto.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/courses/SeatArrangementDto.java
@@ -2,11 +2,14 @@ package edu.kit.informatik.dto.userdata.courses;
 
 import edu.kit.informatik.model.userdata.courses.SeatArrangement;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Data-Transfer-Object zur Klasse {@link SeatArrangement}
@@ -15,11 +18,13 @@ import java.util.Map;
  * @author ugqbo
  * @version 1.0
  */
+@EqualsAndHashCode
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class SeatArrangementDto {
+public class SeatArrangementDto implements Comparable<SeatArrangementDto> {
 
     private String id;
     private String userId;
@@ -27,4 +32,12 @@ public class SeatArrangementDto {
     private Map<String, String> seatMap;
     private String roomId;
     private String courseId;
+
+    @Override
+    public int compareTo(SeatArrangementDto o) {
+        UUID thisUser = UUID.fromString(this.id);
+        UUID oUser = UUID.fromString(o.getId());
+
+        return  thisUser.compareTo(oUser);
+    }
 }
