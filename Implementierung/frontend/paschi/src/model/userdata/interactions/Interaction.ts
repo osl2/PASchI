57ca107aa741bd8bc02f42deeb1d7ac1fdabb03a
uekai <luka.kosak@student.kit.edu>
2023-02-07 13:14:49 +0100

    Added getters and setters to Interaction.ts

--- Implementierung/frontend/paschi/src/model/userdata/interactions/Interaction.ts
+++ Implementierung/frontend/paschi/src/model/userdata/interactions/Interaction.ts
@@ -5,19 +5,54 @@ import {DataObject} from "@/model/DataObject";
 
 export class Interaction extends DataObject {
 
-  user: User;
-  timeStamp: string;
-  fromParticipant: Participant;
-  toParticipant: Participant;
-  category: Category;
+  private readonly _user: User;
+  private readonly _timeStamp: string;
+  private _fromParticipant: Participant;
+  private _toParticipant: Participant;
+  private _category: Category;
 
   constructor(id: string | undefined, localId: number, user: User, timeStamp: string, fromParticipant: Participant,
               toParticipant: Participant, category: Category) {
     super(id, localId);
-    this.user = user;
-    this.timeStamp = timeStamp;
-    this.fromParticipant = fromParticipant;
-    this.toParticipant = toParticipant;
-    this.category = category;
+    this._user = user;
+    this._timeStamp = timeStamp;
+    this._fromParticipant = fromParticipant;
+    this._toParticipant = toParticipant;
+    this._category = category;
+  }
+
+  get user(): User {
+    return this._user;
+  }
+
+  get timeStamp(): string {
+    return this._timeStamp;
+  }
+
+  get fromParticipant(): Participant {
+    return this._fromParticipant;
+  }
+
+  get toParticipant(): Participant {
+    return this._toParticipant;
+  }
+
+  get category(): Category {
+    return this._category;
+  }
+
+  set fromParticipant(value: Participant) {
+    this._fromParticipant = value;
+    this.update();
+  }
+
+  set toParticipant(value: Participant) {
+    this._toParticipant = value;
+    this.update();
+  }
+
+  set category(value: Category) {
+    this._category = value;
+    this.update();
   }
 }
