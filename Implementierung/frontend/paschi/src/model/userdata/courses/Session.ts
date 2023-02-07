714fe7946ad8a690f87878caacc3da8333d54564
uekai <luka.kosak@student.kit.edu>
2023-02-07 11:21:18 +0100

    Added getters and setters to Session.ts

--- Implementierung/frontend/paschi/src/model/userdata/courses/Session.ts
+++ Implementierung/frontend/paschi/src/model/userdata/courses/Session.ts
@@ -6,43 +6,79 @@ import {DataObject} from "@/model/DataObject";
 
 export class Session extends DataObject {
 
-  user: User;
-  name: string;
-  date: string;
-  interactions: Interaction[];
-  course: Course;
-  seatArrangement: SeatArrangement | undefined;
+  private readonly _user: User;
+  private _name: string;
+  private _date: string;
+  private readonly _interactions: Interaction[];
+  private readonly _course: Course;
+  private readonly _seatArrangement: SeatArrangement | undefined;
 
   constructor(id: string | undefined, localId: number, user: User, name: string, date: string, course: Course,
               seatArrangement: SeatArrangement | undefined) {
     super(id, localId);
-    this.user = user;
-    this.date = date;
-    this.name = name;
-    this.course = course;
-    this.seatArrangement = seatArrangement;
-    this.interactions = [];
+    this._user = user;
+    this._date = date;
+    this._name = name;
+    this._course = course;
+    this._seatArrangement = seatArrangement;
+    this._interactions = [];
   }
 
   addInteraction(interaction: Interaction) {
-    this.interactions.push(interaction);
+    this._interactions.push(interaction);
+    this.update();
   }
 
   removeInteraction(interactionId: string) {
-    this.interactions.forEach((element, index) => {
+    this._interactions.forEach((element, index) => {
       if (element.getId == interactionId) {
-        this.interactions.splice(index, 1);
+        this._interactions.splice(index, 1);
       }
     });
+    this.update();
   }
 
   getInteraction(interactionId: string): Interaction | undefined {
-    this.interactions.forEach((element: Interaction) => {
+    this._interactions.forEach((element: Interaction) => {
       if (element.getId == interactionId) {
         return element;
       }
     });
 
     return undefined;
+  }
+
+  get user(): User {
+    return this._user;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get date(): string {
+    return this._date;
+  }
+
+  get interactions(): Interaction[] {
+    return this._interactions;
+  }
+
+  get course(): Course {
+    return this._course;
+  }
+
+  get seatArrangement(): SeatArrangement | undefined {
+    return this._seatArrangement;
+  }
+
+  set name(value: string) {
+    this._name = value;
+    this.update();
+  }
+
+  set date(value: string) {
+    this._date = value;
+    this.update();
   }
 }
