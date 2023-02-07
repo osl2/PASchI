a8c31025c57dda53b243b3aedb06b9c3969bb773
uekai <luka.kosak@student.kit.edu>
2023-02-07 13:19:45 +0100

    Added getters and setters to Position.ts

--- Implementierung/frontend/paschi/src/model/userdata/rooms/Position.ts
+++ Implementierung/frontend/paschi/src/model/userdata/rooms/Position.ts
@@ -3,16 +3,47 @@ import {DataObject} from "@/model/DataObject";
 
 export class Position extends DataObject {
 
-  user: User;
-  xCoordinate: number;
-  yCoordinate: number;
-  orientation: number;
+  private readonly _user: User;
+  private _xCoordinate: number;
+  private _yCoordinate: number;
+  private _orientation: number;
 
   constructor(id: string | undefined, localId: number, user: User, xCoordinate: number, yCoordinate: number, orientation: number) {
     super(id, localId);
-    this.user = user;
-    this.xCoordinate = xCoordinate;
-    this.yCoordinate = yCoordinate;
-    this.orientation = orientation;
+    this._user = user;
+    this._xCoordinate = xCoordinate;
+    this._yCoordinate = yCoordinate;
+    this._orientation = orientation;
+  }
+
+  get user(): User {
+    return this._user;
+  }
+
+  get xCoordinate(): number {
+    return this._xCoordinate;
+  }
+
+  get yCoordinate(): number {
+    return this._yCoordinate;
+  }
+
+  get orientation(): number {
+    return this._orientation;
+  }
+
+  set xCoordinate(value: number) {
+    this._xCoordinate = value;
+    this.update();
+  }
+
+  set yCoordinate(value: number) {
+    this._yCoordinate = value;
+    this.update();
+  }
+
+  set orientation(value: number) {
+    this._orientation = value;
+    this.update();
   }
 }
