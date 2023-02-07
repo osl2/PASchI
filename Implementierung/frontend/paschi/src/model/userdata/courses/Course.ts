b7d4e9171c11d2041224e5675e0c5a180e465c4c
uekai <luka.kosak@student.kit.edu>
2023-02-07 11:19:33 +0100

    Added getters and setters to Course.ts

--- Implementierung/frontend/paschi/src/model/userdata/courses/Course.ts
+++ Implementierung/frontend/paschi/src/model/userdata/courses/Course.ts
@@ -6,37 +6,39 @@ import {DataObject} from "@/model/DataObject";
 
 export class Course extends DataObject {
 
-  user: User
-  name: string;
-  subject: string;
-  participants: Participant[];
-  sessions: Session[];
-  seatArrangements: SeatArrangement[];
+  private readonly _user: User
+  private _name: string;
+  private _subject: string;
+  private readonly _participants: Participant[];
+  private readonly _sessions: Session[];
+  private readonly _seatArrangements: SeatArrangement[];
 
   constructor(id: string | undefined, localId: number, user: User, name: string, subject: string) {
     super(id, localId);
-    this.user = user;
-    this.name = name;
-    this.subject = subject;
-    this.participants = [];
-    this.sessions = [];
-    this.seatArrangements = [];
+    this._user = user;
+    this._name = name;
+    this._subject = subject;
+    this._participants = [];
+    this._sessions = [];
+    this._seatArrangements = [];
   }
 
   addParticipant(participant: Participant) {
-    this.participants.push(participant);
+    this._participants.push(participant);
+    this.update();
   }
 
   removeParticipant(participantId: string) {
-    this.participants.forEach((element, index) => {
+    this._participants.forEach((element, index) => {
       if (element.getId == participantId) {
-        this.participants.splice(index, 1)
+        this._participants.splice(index, 1)
       }
     });
+    this.update();
   }
 
   getParticipant(participantId: string): Participant | undefined {
-    this.participants.forEach((participant: Participant) => {
+    this._participants.forEach((participant: Participant) => {
       if (participant.getId === participantId) {
         return participant;
       }
@@ -46,26 +48,64 @@ export class Course extends DataObject {
   }
 
   addSession(session: Session) {
-    this.sessions.push(session);
+    this._sessions.push(session);
+    this.update();
   }
 
   removeSession(sessionId: string) {
-    this.sessions.forEach((element, index) => {
+    this._sessions.forEach((element, index) => {
       if (element.getId == sessionId) {
-        this.sessions.splice(index, 1);
+        this._sessions.splice(index, 1);
       }
     });
+    this.update();
   }
 
   addSeatArrangement(seatArrangement: SeatArrangement) {
-    this.seatArrangements.push(seatArrangement);
+    this._seatArrangements.push(seatArrangement);
+    this.update();
   }
 
   removeSeatArrangement(arrangementId: string) {
-    this.seatArrangements.forEach((element, index) => {
+    this._seatArrangements.forEach((element, index) => {
       if (element.getId === arrangementId) {
-        this.seatArrangements.splice(index, 1);
+        this._seatArrangements.splice(index, 1);
       }
     });
+    this.update();
+  }
+
+  get user(): User {
+    return this._user;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get subject(): string {
+    return this._subject;
+  }
+
+  get participants(): Participant[] {
+    return this._participants;
+  }
+
+  get sessions(): Session[] {
+    return this._sessions;
+  }
+
+  get seatArrangements(): SeatArrangement[] {
+    return this._seatArrangements;
+  }
+
+  set name(value: string) {
+    this._name = value;
+    this.update();
+  }
+
+  set subject(value: string) {
+    this._subject = value;
+    this.update();
   }
 }
