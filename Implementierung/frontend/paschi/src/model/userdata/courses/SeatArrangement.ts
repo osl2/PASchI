948b6b83f59d5bfbb0c3d83f559d910d191a42fb
uekai <luka.kosak@student.kit.edu>
2023-02-07 11:20:46 +0100

    Added getters and setters to SeatArrangement.ts

--- Implementierung/frontend/paschi/src/model/userdata/courses/SeatArrangement.ts
+++ Implementierung/frontend/paschi/src/model/userdata/courses/SeatArrangement.ts
@@ -7,30 +7,57 @@ import {DataObject} from "@/model/DataObject";
 
 export class SeatArrangement extends DataObject {
 
-  user: User
-  name: string;
-  seatMap: Map<RoomObject, Participant>;
-  course: Course;
-  room: Room;
+  private readonly _user: User
+  private _name: string;
+  private readonly _seatMap: Map<RoomObject, Participant>;
+  private readonly _course: Course;
+  private readonly _room: Room;
 
   constructor(id: string | undefined, localId: number, user: User, name: string, course: Course, room: Room) {
     super(id, localId);
-    this.user = user;
-    this.name = name;
-    this.seatMap = new Map<RoomObject, Participant>();
-    this.course = course;
-    this.room = room;
+    this._user = user;
+    this._name = name;
+    this._seatMap = new Map<RoomObject, Participant>();
+    this._course = course;
+    this._room = room;
   }
 
   getParticipantForSeat(seat: RoomObject): Participant | undefined {
-    return this.seatMap.get(seat);
+    return this._seatMap.get(seat);
   }
 
   setSeat(seat: RoomObject, participant: Participant) {
-    this.seatMap.set(seat, participant);
+    this._seatMap.set(seat, participant);
+    this.update();
   }
 
   removeSeat(seat: RoomObject) {
-    this.seatMap.delete(seat);
+    this._seatMap.delete(seat);
+    this.update();
+  }
+
+  get user(): User {
+    return this._user;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get seatMap(): Map<RoomObject, Participant> {
+    return this._seatMap;
+  }
+
+  get course(): Course {
+    return this._course;
+  }
+
+  get room(): Room {
+    return this._room;
+  }
+
+  set name(value: string) {
+    this._name = value;
+    this.update();
   }
 }
