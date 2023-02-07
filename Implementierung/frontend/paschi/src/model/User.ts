dc1f64e1c4d515e7e8fca768938eb680ce1eb4c8
uekai <luka.kosak@student.kit.edu>
2023-02-07 11:19:13 +0100

    Added getters and setters to User.ts

--- Implementierung/frontend/paschi/src/model/User.ts
+++ Implementierung/frontend/paschi/src/model/User.ts
@@ -3,21 +3,75 @@ import {DataObject} from "@/model/DataObject";
 
 export class User extends DataObject {
 
-  firstName: string;
-  lastName: string;
-  email: string;
-  auth: boolean;
-  role: Role;
-  token: string | undefined;
+  private _firstName: string;
+  private _lastName: string;
+  private _email: string;
+  private _auth: boolean;
+  private _role: Role;
+  private _token: string | undefined;
 
   constructor(id: string | undefined, localId: number, firstName: string, lastName: string, email: string,
               auth: boolean, role: Role, token: string | undefined) {
     super(id, localId);
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.auth = auth;
-    this.role = role;
-    this.token = token;
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._email = email;
+    this._auth = auth;
+    this._role = role;
+    this._token = token;
+  }
+
+  get firstName(): string {
+    return this._firstName;
+  }
+
+  get lastName(): string {
+    return this._lastName;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get auth(): boolean {
+    return this._auth;
+  }
+
+  get role(): Role {
+    return this._role;
+  }
+
+  get token(): string | undefined {
+    return this._token;
+  }
+
+  set firstName(value: string) {
+    this._firstName = value;
+    this.update();
+  }
+
+  set lastName(value: string) {
+    this._lastName = value;
+    this.update();
+  }
+
+  set email(value: string) {
+    this._email = value;
+    this.update();
+  }
+
+  set auth(value: boolean) {
+    this._auth = value;
+    this.update();
+  }
+
+  set role(value: Role) {
+    this._role = value;
+    this.update();
+  }
+
+  set token(value: string | undefined) {
+    this._token = value;
+    this.update();
   }
 }
