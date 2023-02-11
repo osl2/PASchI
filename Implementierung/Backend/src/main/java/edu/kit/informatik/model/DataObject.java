6f35d0d55df15af0b4ecbd90b1d90398bc04775c
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-11 16:00:32 +0100

    Added new abstract DataObject for Inherited Attributes

---
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/model/DataObject.java
@@ -0,0 +1,40 @@
+package edu.kit.informatik.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class DataObject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne
+    private User user;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+    public DataObject(User user, Timestamp createdAt, Timestamp updatedAt) {
+        this.user = user;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+
+}
