dd470e097da4229ec02d3e17d0f353812b178576
uekai <luka.kosak@student.kit.edu>
2023-02-12 19:31:31 +0100

    Implemented RoomService.ts

--- Implementierung/frontend/paschi/src/service/RoomService.ts
+++ Implementierung/frontend/paschi/src/service/RoomService.ts
@@ -2,7 +2,9 @@ import {BaseService} from "@/service/BaseService";
 import {Room} from "@/model/userdata/rooms/Room";
 import {RoomDto} from "@/dto/userdata/rooms/RoomDto";
 import {RoomMapper} from "@/dto/mapper/rooms/RoomMapper";
+import axios from "axios/index";
 
+// TODO: URL
 const ROOM_BASE_URL: string = '';
 
 export class RoomService extends BaseService<Room, RoomDto> {
@@ -11,18 +13,47 @@ export class RoomService extends BaseService<Room, RoomDto> {
     super(RoomMapper.getMapper());
   }
 
-  add(e: Room) {
+  add(room: Room) {
+    const roomDto = this.getMapper().modelToDto(room);
+    axios.post(ROOM_BASE_URL + '', roomDto).then((response) => {
+      // irgendwas
+    });
   }
 
-  update(e: Room) {
+  update(room: Room) {
+    const roomDto = this.getMapper().modelToDto(room);
+    axios.post(ROOM_BASE_URL + '', roomDto).then((response) => {
+      // irgendwas
+    });
   }
 
-  getById(id: string): Room | undefined {
+  async getById(id: string): Promise<Room | undefined> {
+    let room;
+    await axios.get(ROOM_BASE_URL + '').then((response) => {
+      room = this.getMapper().dtoToModel(response.data);
+    });
+
+    if (room != undefined) {
+      return room;
+    } else {
+      return undefined;
+    }
   }
 
-  getAll(): Room[] {
+  async getAll(): Promise<Room[]> {
+    let rooms: Room[] = [];
+    axios.get(ROOM_BASE_URL + '').then((response) => {
+      response.data.forEach((course: any) => {
+        rooms.push(this.getMapper().dtoToModel(course));
+      });
+    });
+
+    return rooms;
   }
 
   delete(id: string) {
+    axios.delete(ROOM_BASE_URL + '').then((response) => {
+      // irgendwas
+    });
   }
 }
