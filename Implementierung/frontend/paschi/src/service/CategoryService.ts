6ce3ce59609546bd02413c4b0c3e9fb7f4f1d70b
uekai <luka.kosak@student.kit.edu>
2023-02-12 19:17:58 +0100

    Implemented CategoryService.ts

--- Implementierung/frontend/paschi/src/service/CategoryService.ts
+++ Implementierung/frontend/paschi/src/service/CategoryService.ts
@@ -2,27 +2,58 @@ import {BaseService} from "@/service/BaseService";
 import {Category} from "@/model/userdata/interactions/Category";
 import {CategoryDto} from "@/dto/userdata/interactions/CategoryDto";
 import {CategoryMapper} from "@/dto/mapper/interactions/CategoryMapper";
+import axios from "axios/index";
 
 const CATEGORY_BASE_URL: string = '';
 
+// TODO: URL
 export class CategoryService extends BaseService<Category, CategoryDto> {
 
   constructor() {
     super(CategoryMapper.getMapper());
   }
 
-  add(e: Category) {
+  add(category: Category) {
+    const categoryDto = this.getMapper().modelToDto(category);
+    axios.post(CATEGORY_BASE_URL + '', categoryDto).then((response) => {
+      // irgendwas
+    });
   }
 
-  update(e: Category) {
+  update(category: Category) {
+    const categoryDto = this.getMapper().modelToDto(category);
+    axios.post(CATEGORY_BASE_URL + '', categoryDto).then((response) => {
+      // irgendwas
+    });
   }
 
-  getById(id: string): Category | undefined {
+  async getById(id: string): Promise<Category | undefined> {
+    let category;
+    await axios.get(CATEGORY_BASE_URL + '').then((response) => {
+      category = this.getMapper().dtoToModel(response.data);
+    });
+
+    if (category != undefined) {
+      return category;
+    } else {
+      return undefined;
+    }
   }
 
-  getAll(): Category[] {
+  async getAll(): Promise<Category[]> {
+    let categories: Category[] = [];
+    axios.get(CATEGORY_BASE_URL + '').then((response) => {
+      response.data.forEach((course: any) => {
+        categories.push(this.getMapper().dtoToModel(course));
+      });
+    });
+
+    return categories;
   }
 
   delete(id: string) {
+    axios.delete(CATEGORY_BASE_URL + '').then((response) => {
+      // irgendwas
+    });
   }
 }
