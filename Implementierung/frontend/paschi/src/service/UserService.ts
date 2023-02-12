26f74ffb078742a571065a3827de72c7da9e753e
uekai <luka.kosak@student.kit.edu>
2023-02-12 20:11:48 +0100

    Implemented UserService.ts

--- Implementierung/frontend/paschi/src/service/UserService.ts
+++ Implementierung/frontend/paschi/src/service/UserService.ts
@@ -1,6 +1,8 @@
 import {BaseService} from "@/service/BaseService";
 import {User} from "@/model/User";
 import {UserDto} from "@/dto/UserDto";
+import axios from "axios/index";
+import {UserMapper} from "@/dto/mapper/UserMapper";
 
 const USER_BASE_URL: string = '';
 
@@ -11,18 +13,47 @@ export class UserService extends BaseService<User, UserDto> {
   }
 
   add(user: User) {
+    const userDto = this.getMapper().modelToDto(user);
+    axios.post(USER_BASE_URL + '', userDto).then((response) => {
+      // irgendwas
+    });
   }
 
   update(user: User) {
+    const userDto = this.getMapper().modelToDto(user);
+    axios.post(USER_BASE_URL + '', userDto).then((response) => {
+      // irgendwas
+    });
   }
 
-  getById(id: string): User {
+  async getById(id: string): Promise<User | undefined> {
+    let user;
+    await axios.get(USER_BASE_URL + '').then((response) => {
+      user = this.getMapper().dtoToModel(response.data);
+    });
+
+    if (user != undefined) {
+      return user;
+    } else {
+      return undefined;
+    }
   }
 
-  getAll(): User[] {
+  async getAll(): Promise<User[]> {
+    let users: User[] = [];
+    axios.get(USER_BASE_URL + '').then((response) => {
+      response.data.forEach((course: any) => {
+        users.push(this.getMapper().dtoToModel(course));
+      });
+    });
+
+    return users;
   }
 
   delete(id: string) {
+    axios.delete(USER_BASE_URL + '').then((response) => {
+      // irgendwas
+    });
   }
 
   // TODO: login
