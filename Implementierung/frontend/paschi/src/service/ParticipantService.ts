0c5d006ef3c0e5122d6c2d241644c58d1f19c203
uekai <luka.kosak@student.kit.edu>
2023-02-12 19:26:34 +0100

    Implemented ParticipantService.ts

--- Implementierung/frontend/paschi/src/service/ParticipantService.ts
+++ Implementierung/frontend/paschi/src/service/ParticipantService.ts
@@ -2,7 +2,9 @@ import {BaseService} from "@/service/BaseService";
 import {Participant} from "@/model/userdata/interactions/Participant";
 import {ParticipantDto} from "@/dto/userdata/interactions/ParticipantDto";
 import {ParticipantMapper} from "@/dto/mapper/interactions/ParticipantMapper";
+import axios from "axios";
 
+// TODO: URL
 const PARTICIPANT_BASE_URL: string = '';
 
 export class ParticipantService extends BaseService<Participant, ParticipantDto> {
@@ -11,18 +13,47 @@ export class ParticipantService extends BaseService<Participant, ParticipantDto>
     super(ParticipantMapper.getMapper());
   }
 
-  add(e: Participant) {
+  add(participant: Participant) {
+    const participantDto = this.getMapper().modelToDto(participant);
+    axios.post(PARTICIPANT_BASE_URL + '', participantDto).then((response) => {
+      // irgendwas
+    });
   }
 
-  update(e: Participant) {
+  update(participant: Participant) {
+    const participantDto = this.getMapper().modelToDto(participant);
+    axios.post(PARTICIPANT_BASE_URL + '', participantDto).then((response) => {
+      // irgendwas
+    });
   }
 
-  getById(id: string): Participant {
+  async getById(id: string): Promise<Participant | undefined> {
+    let participant;
+    await axios.get(PARTICIPANT_BASE_URL + '').then((response) => {
+      participant = this.getMapper().dtoToModel(response.data);
+    });
+
+    if (participant != undefined) {
+      return participant;
+    } else {
+      return undefined;
+    }
   }
 
-  getAll(): Participant[] {
+  async getAll(): Promise<Participant[]> {
+    let participants: Participant[] = [];
+    axios.get(PARTICIPANT_BASE_URL + '').then((response) => {
+      response.data.forEach((course: any) => {
+        participants.push(this.getMapper().dtoToModel(course));
+      });
+    });
+
+    return participants;
   }
 
   delete(id: string) {
+    axios.delete(PARTICIPANT_BASE_URL + '').then((response) => {
+      // irgendwas
+    });
   }
 }
