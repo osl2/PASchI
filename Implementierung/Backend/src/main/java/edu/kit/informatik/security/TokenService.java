34d9d3989a09e5b9f12855fb24ca264ab9390efb
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-13 16:11:54 +0100

    Added TokenService to generate JWT Tokens

--- Implementierung/Backend/src/main/java/edu/kit/informatik/security/TokenService.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/security/TokenService.java
@@ -1,4 +1,41 @@
 package edu.kit.informatik.security;
 
+import edu.kit.informatik.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+
+@Service
 public class TokenService {
+    private final JwtEncoder jwtEncoder;
+
+    public TokenService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    public String generateToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+
+        Instant now = Instant.now();
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.HOURS))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .claim("userId", user.getId())
+                .build();
+        //System.out.println(claims.getClaims().values());
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
 }
