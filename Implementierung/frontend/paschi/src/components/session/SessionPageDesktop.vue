7fc19a384716d4c80d3ecb8149a6ddbfdcb58618
Cian Payne <cian.payne@student.kit.edu>
2023-02-14 09:42:24 +0100

    Auto stash before rebase of "origin/Implementierung/frontend"

---
+++ Implementierung/frontend/paschi/src/components/session/SessionPageDesktop.vue
@@ -0,0 +1,38 @@
+<template>
+  <RoomDisplay :room-id="roomId">
+    <template v-slot:chair="chair">
+      <SeatLabel :chair="chair.chair" :participant="seatArrangement.getParticipantForSeat(chair.chair)" />
+    </template>
+  </RoomDisplay>
+</template>
+
+<script>
+import { SessionController } from '@/controller/SessionController';
+import RoomDisplay from '@/components/room/RoomDisplay.vue'
+import SeatLabel from "@/components/room/SeatLabel.vue";
+
+export default defineComponent({
+  name: 'SessionPageDesktop',
+  components: {SeatLabel, RoomDisplay},
+  props: {
+    sessionId: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const sessionId = props.sessionId;
+    const sessionController = SessionController.getSessionController();
+    const session = sessionController.getSession(sessionId);
+    const seatArrangement = sessionController.getSeatArrangementOfSession(sessionId);
+
+    return {
+      roomId: seatArrangement.room.getId,
+      seatArrangement
+    }
+
+  },
+});
+</script>
+
+<style scoped></style>
