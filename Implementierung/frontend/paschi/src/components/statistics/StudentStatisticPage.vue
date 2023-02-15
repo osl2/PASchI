b0b92119b9f80d2bbce59197da93ce80ce451aff
Aaron Sutor <unvil@student.kit.edu>
2023-02-15 18:48:22 +0100

    student statistiken "quasi fertig"

--- Implementierung/frontend/paschi/src/components/statistics/StudentStatisticPage.vue
+++ Implementierung/frontend/paschi/src/components/statistics/StudentStatisticPage.vue
@@ -1,15 +1,40 @@
 <template>
-  <NavigationBar extended />
+  <NavigationBar>
+    <template #default>
+      <v-app-bar-title>
+        Schülerstatistiken von {{firstName + " " + lastName}}
+      </v-app-bar-title>
+    </template>
+  </NavigationBar>
   <v-main>
     <SideMenu />
     <v-container fluid>
       <v-row justify="space-around" align-content="stretch">
         <v-col>
-          <h3> Durchschnittliche Qualität </h3>
-          {{statsController.getStudentStats(studentId)[1]}} / 5
+          <v-card color="primary-lighten-1">
+            <v-card-title>
+              <v-row class="ma-2">
+                Durchschnittliche Qualität der Beiträge
+                <v-spacer />
+              </v-row>
+            </v-card-title>
+            <v-card-item>
+              {{statsController.getStudentStats(studentId)[1]}} / 5
+            </v-card-item>
+          </v-card>
         </v-col>
         <v-col>
-          <canvas id = "categoryChart" width="100" />
+          <v-card color="primary-lighten-1">
+            <v-card-title>
+              <v-row class="ma-2">
+                Aufschlüsselung der Interaktionen nach Kategorien
+                <v-spacer />
+              </v-row>
+            </v-card-title>
+            <v-card-item>
+              <canvas id = "categoryChart"/>
+            </v-card-item>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -18,11 +43,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import {defineComponent, ref, Ref} from "vue";
 import NavigationBar from "@/components/navigation/NavigationBar.vue";
 import SideMenu from "@/components/navigation/SideMenu.vue";
 import Chart from 'chart.js/auto';
 import {StatsController} from "@/controller/StatsController";
+import {StudentController} from "@/controller/StudentController";
+import {Student} from "@/model/userdata/interactions/Student";
 
 
 
@@ -38,11 +65,16 @@ export default defineComponent({
     },
   },
 
-  setup(){
+  setup(props){
     const statsController = StatsController.getStatsController();
+    const studentController = StudentController.getStudentConroller();
+    const firstName = studentController.getStudent(props.studentId)?.firstName;
+    const lastName = studentController.getStudent(props.studentId)?.lastName;
+
+
 
     return {
-      statsController,
+      statsController, firstName, lastName
     };
     /*const saveStatisticsClick {
 
@@ -70,19 +102,31 @@ export default defineComponent({
 
     const data = {
       labels:
-      //['test', 'test2', 'test3'],
-        keys,
+      ['Störungsdummy', 'Antwortdummy', 'Fragendummy'],
+        //keys,
       datasets: [{
         label: 'Kategorien',
         data:
-        //[1, 2, 3],
-        values,
+        [5,47,19],
+        //values,
       }]
     };
 
     const categoryChart = new Chart(categoryChartId, {
       type: 'pie',
       data: data,
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: true,
+            labels: {
+              color: 'rgb(255, 255, 255)'
+            }
+          }
+        }
+      }
 
     });
 
