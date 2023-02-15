7f583c458d299efe5452f1f1e01d8bf292bfc543
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-15 17:58:57 +0100

    Used String.format()

--- Implementierung/Backend/src/main/java/edu/kit/informatik/exceptions/EntityNotFoundException.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/exceptions/EntityNotFoundException.java
@@ -1,11 +1,12 @@
 package edu.kit.informatik.exceptions;
 
-import edu.kit.informatik.model.DataObject;
-
 public class EntityNotFoundException extends IllegalArgumentException {
 
+    private static final String OUTPUT = "Entity of class '%s' with id: '%s' not found";
+
     public EntityNotFoundException(Class<?> tClass, String id) {
-        super("Entity of class '" + tClass.getSimpleName() + "' with id: '" + id + "' not found");
+        super(String.format(OUTPUT, tClass.getSimpleName(), id));
+        //super("Entity of class '" + tClass.getSimpleName() + "' with id: '" + id + "' not found");
     }
 
 }
