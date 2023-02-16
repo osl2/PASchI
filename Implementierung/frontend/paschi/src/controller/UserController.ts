1d2c1c3419dbe79a426f06f12964e98d2f5199bb
uekai <luka.kosak@student.kit.edu>
2023-02-16 13:03:17 +0100

    Added default categories

--- Implementierung/frontend/paschi/src/controller/UserController.ts
+++ Implementierung/frontend/paschi/src/controller/UserController.ts
@@ -2,11 +2,13 @@ import {User} from "@/model/User";
 import {useUserStore} from "@/store/UserStore";
 import {Role} from "@/model/Role";
 import {createPinia} from "pinia";
+import {CategoryController} from "@/controller/CategoryController";
 
 // TODO: Services fürs Backend einbinden
 export class UserController {
 
   private static controller: UserController = new UserController();
+  private categoryController: CategoryController = CategoryController.getCategoryController();
   private userStore = useUserStore(createPinia());
 
   private constructor() {
@@ -25,6 +27,9 @@ export class UserController {
 
   register(firstName: string, lastName: string, email: string, password: string, repeatPassword: string) {
     // TODO: auth vom admin setzen
+    this.categoryController.createCategory("Störung");
+    this.categoryController.createRatedCategory("Antwort");
+    this.categoryController.createRatedCategory("Frage");
     this.userStore.setUser(
       new User(
         undefined,
