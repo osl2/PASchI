ed0314cee9828444e28b65d2ce0d154568780a5a
uekai <luka.kosak@student.kit.edu>
2023-02-17 16:53:09 +0100

    Implemented RoomService.ts

--- Implementierung/frontend/paschi/src/service/RoomService.ts
+++ Implementierung/frontend/paschi/src/service/RoomService.ts
@@ -2,14 +2,15 @@ import {BaseService} from "@/service/BaseService";
 import {Room} from "@/model/userdata/rooms/Room";
 import {RoomDto} from "@/dto/userdata/rooms/RoomDto";
 import {RoomMapper} from "@/dto/mapper/rooms/RoomMapper";
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
+import {useUserStore} from "@/store/UserStore";
 
-// TODO: URL
-const ROOM_BASE_URL: string = '';
+const ROOM_BASE_URL: string = 'http://193.196.37.141/api/room';
 
 export class RoomService extends BaseService<Room, RoomDto> {
 
   private static roomService: RoomService = new RoomService();
+  private userStore = useUserStore();
 
   private constructor() {
     super(RoomMapper.getMapper());
@@ -21,22 +22,34 @@ export class RoomService extends BaseService<Room, RoomDto> {
 
   add(room: Room) {
     const roomDto = this.getMapper().modelToDto(room);
-    axios.post(ROOM_BASE_URL + '', roomDto).then((response) => {
-      // irgendwas
+    axios.post(ROOM_BASE_URL, roomDto).catch((error) => {
+      console.log(error);
     });
   }
 
   update(room: Room) {
+    const token = this.userStore.getUser()?.token;
     const roomDto = this.getMapper().modelToDto(room);
-    axios.post(ROOM_BASE_URL + '', roomDto).then((response) => {
-      // irgendwas
+    axios.put(ROOM_BASE_URL, roomDto, {
+      headers: {
+        'Authorization': token
+      }
+    }).catch((error) => {
+      console.log(error);
     });
   }
 
   async getById(id: string): Promise<Room | undefined> {
+    const token = this.userStore.getUser()?.token;
     let room;
-    await axios.get(ROOM_BASE_URL + '').then((response) => {
+    await axios.get(ROOM_BASE_URL + `/${id}`, {
+      headers: {
+        'Authorization': token
+      }
+    }).then((response: AxiosResponse<RoomDto>) => {
       room = this.getMapper().dtoToModel(response.data);
+    }).catch((error) => {
+      console.log(error);
     });
 
     if (room != undefined) {
@@ -47,19 +60,34 @@ export class RoomService extends BaseService<Room, RoomDto> {
   }
 
   async getAll(): Promise<Room[]> {
+    const token = this.userStore.getUser()?.token;
     let rooms: Room[] = [];
-    axios.get(ROOM_BASE_URL + '').then((response) => {
-      response.data.forEach((course: any) => {
-        rooms.push(this.getMapper().dtoToModel(course));
+    axios.get(ROOM_BASE_URL, {
+      headers: {
+        'Authorization': token
+      }
+    }).then((response: AxiosResponse<RoomDto[]>) => {
+      response.data.forEach((roomDto: RoomDto) => {
+        rooms.push(this.getMapper().dtoToModel(roomDto));
       });
+    }).catch((error) => {
+      console.log(error);
     });
 
     return rooms;
   }
 
   delete(id: string) {
-    axios.delete(ROOM_BASE_URL + '').then((response) => {
-      // irgendwas
+    const token = this.userStore.getUser()?.token;
+    axios.delete(ROOM_BASE_URL, {
+      params: {
+        id
+      },
+      headers: {
+        'Authorization': token
+      }
+    }).catch((error) => {
+      console.log(error);
     });
   }
 }
