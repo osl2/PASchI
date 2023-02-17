d6e4ec52560f19d7165f3c2d548db4065db8966e
uekai <luka.kosak@student.kit.edu>
2023-02-17 23:10:21 +0100

    Implemented PositionMapper.ts

--- Implementierung/frontend/paschi/src/dto/mapper/rooms/PositionMapper.ts
+++ Implementierung/frontend/paschi/src/dto/mapper/rooms/PositionMapper.ts
@@ -1,10 +1,14 @@
 import {IModelDtoMapper} from "@/dto/mapper/IModelDtoMapper";
 import {Position} from "@/model/userdata/rooms/Position";
 import {PositionDto} from "@/dto/userdata/rooms/PositionDto";
+import {UserController} from "@/controller/UserController";
+import {usePositionStore} from "@/store/PositionStore";
 
 export class PositionMapper implements IModelDtoMapper<Position, PositionDto> {
 
   private static mapper: PositionMapper = new PositionMapper();
+  private userController = UserController.getUserController();
+  private positionStore = usePositionStore();
 
   private constructor() {
   }
@@ -13,12 +17,41 @@ export class PositionMapper implements IModelDtoMapper<Position, PositionDto> {
     return PositionMapper.mapper;
   }
 
-  dtoToModel(positionDto: PositionDto): Position {
-    return undefined;
-  }
-
   modelToDto(position: Position): PositionDto {
-    return undefined;
+    return new PositionDto(
+      position.getId,
+      this.userController.getUser().getId,
+      position.createdAt,
+      position.updatedAt,
+      position.xCoordinate,
+      position.yCoordinate,
+      position.orientation
+    );
   }
 
+  dtoToModel(positionDto: PositionDto): Position {
+    let position = this.positionStore.getPosition(positionDto.id);
+    if (position == undefined) {
+      position = new Position(
+        positionDto.id,
+        0,
+        this.userController.getUser(),
+        positionDto.xCoordinate,
+        positionDto.yCoordinate,
+        positionDto.orientation
+      );
+      this.positionStore.addPosition(position);
+    } else if (positionDto.updatedAt <= position.updatedAt && positionDto.createdAt >= position.createdAt) {
+      return position;
+    } else {
+      position.xCoordinate = positionDto.xCoordinate;
+      position.yCoordinate = positionDto.yCoordinate;
+      position.orientation = positionDto.orientation;
+    }
+    if (positionDto.createdAt < position.createdAt) {
+      this.positionStore.addPosition(position);
+    }
+
+    return position;
+  }
 }
