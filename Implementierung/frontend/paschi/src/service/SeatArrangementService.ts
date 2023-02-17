fe8fde0e74c62ec90f6eea776959d8f154507084
uekai <luka.kosak@student.kit.edu>
2023-02-17 17:01:49 +0100

    Implemented SeatArrangementService.ts

--- Implementierung/frontend/paschi/src/service/SeatArrangementService.ts
+++ Implementierung/frontend/paschi/src/service/SeatArrangementService.ts
@@ -2,14 +2,15 @@ import {BaseService} from "@/service/BaseService";
 import {SeatArrangement} from "@/model/userdata/courses/SeatArrangement";
 import {SeatArrangementDto} from "@/dto/userdata/courses/SeatArrangementDto";
 import {SeatArrangementMapper} from "@/dto/mapper/courses/SeatArrangementMapper";
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
+import {useUserStore} from "@/store/UserStore";
 
-// TODO: URL
-const SEAT_ARRANGEMENT_BASE_URL: string = '';
+const SEAT_ARRANGEMENT_BASE_URL: string = 'http://193.196.37.141/api/seatarrangement';
 
 export class SeatArrangementService extends BaseService<SeatArrangement, SeatArrangementDto> {
 
   private static seatArrangementService: SeatArrangementService = new SeatArrangementService();
+  private userStore = useUserStore();
 
   private constructor() {
     super(SeatArrangementMapper.getMapper());
@@ -21,22 +22,34 @@ export class SeatArrangementService extends BaseService<SeatArrangement, SeatArr
 
   add(arrangement: SeatArrangement) {
     const arrangementDto = this.getMapper().modelToDto(arrangement);
-    axios.post(SEAT_ARRANGEMENT_BASE_URL + '', arrangementDto).then((response) => {
-      // irgendwas
+    axios.post(SEAT_ARRANGEMENT_BASE_URL, arrangementDto).catch((error) => {
+      console.log(error);
     });
   }
 
   update(arrangement: SeatArrangement) {
+    const token = this.userStore.getUser()?.token;
     const arrangementDto = this.getMapper().modelToDto(arrangement);
-    axios.post(SEAT_ARRANGEMENT_BASE_URL + '', arrangementDto).then((response) => {
-      // irgendwas
+    axios.put(SEAT_ARRANGEMENT_BASE_URL, arrangementDto, {
+      headers: {
+        'Authorization': token
+      }
+    }).catch((error) => {
+      console.log(error);
     });
   }
 
   async getById(id: string): Promise<SeatArrangement | undefined> {
+    const token = this.userStore.getUser()?.token;
     let arrangement;
-    await axios.get(SEAT_ARRANGEMENT_BASE_URL + '').then((response) => {
+    await axios.get(SEAT_ARRANGEMENT_BASE_URL + `/${id}`, {
+      headers: {
+        'Authorization': token
+      }
+    }).then((response: AxiosResponse<SeatArrangementDto>) => {
       arrangement = this.getMapper().dtoToModel(response.data);
+    }).catch((error) => {
+      console.log(error);
     });
 
     if (arrangement != undefined) {
@@ -47,19 +60,34 @@ export class SeatArrangementService extends BaseService<SeatArrangement, SeatArr
   }
 
   async getAll(): Promise<SeatArrangement[]> {
+    const token = this.userStore.getUser()?.token;
     let arrangements: SeatArrangement[] = [];
-    axios.get(SEAT_ARRANGEMENT_BASE_URL + '').then((response) => {
-      response.data.forEach((course: any) => {
-        arrangements.push(this.getMapper().dtoToModel(course));
+    axios.get(SEAT_ARRANGEMENT_BASE_URL, {
+      headers: {
+        'Authorization': token
+      }
+    }).then((response: AxiosResponse<SeatArrangementDto[]>) => {
+      response.data.forEach((arrangementDto: SeatArrangementDto) => {
+        arrangements.push(this.getMapper().dtoToModel(arrangementDto));
       });
+    }).catch((error) => {
+      console.log(error);
     });
 
     return arrangements;
   }
 
   delete(id: string) {
-    axios.delete(SEAT_ARRANGEMENT_BASE_URL + '').then((response) => {
-      // irgendwas
+    const token = this.userStore.getUser()?.token;
+    axios.delete(SEAT_ARRANGEMENT_BASE_URL, {
+      params: {
+        id
+      },
+      headers: {
+        'Authorization': token
+      }
+    }).catch((error) => {
+      console.log(error);
     });
   }
 }
