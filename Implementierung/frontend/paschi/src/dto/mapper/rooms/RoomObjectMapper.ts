609bf3da610101a963959447166f8e3e2025673a
uekai <luka.kosak@student.kit.edu>
2023-02-19 19:28:47 +0100

    Implemented modelToDto() in RoomObjectMapper.ts

--- Implementierung/frontend/paschi/src/dto/mapper/rooms/RoomObjectMapper.ts
+++ Implementierung/frontend/paschi/src/dto/mapper/rooms/RoomObjectMapper.ts
@@ -1,10 +1,12 @@
 import {IModelDtoMapper} from "@/dto/mapper/IModelDtoMapper";
 import {RoomObject} from "@/model/userdata/rooms/RoomObject";
 import {RoomObjectDto} from "@/dto/userdata/rooms/RoomObjectDto";
+import {PositionMapper} from "@/dto/mapper/rooms/PositionMapper";
 
 export class RoomObjectMapper implements IModelDtoMapper<RoomObject, RoomObjectDto> {
 
   private static mapper: RoomObjectMapper = new RoomObjectMapper();
+  private positionMapper = PositionMapper.getMapper();
 
   private constructor() {
   }
@@ -13,12 +15,17 @@ export class RoomObjectMapper implements IModelDtoMapper<RoomObject, RoomObjectD
     return RoomObjectMapper.mapper;
   }
 
+  modelToDto(roomObject: RoomObject): RoomObjectDto {
+    return new RoomObjectDto(
+      roomObject.getId,
+      roomObject.user.getId,
+      roomObject.createdAt,
+      roomObject.updatedAt,
+      this.positionMapper.modelToDto(roomObject.position)
+    );
+  }
+
   dtoToModel(roomObjectDto: RoomObjectDto): RoomObject {
     return undefined;
   }
-
-  modelToDto(roomObject: RoomObject): RoomObjectDto {
-    return undefined;
-  }
-
 }
