32b7d849b974650a21dd1c49a05256411104f069
uekai <luka.kosak@student.kit.edu>
2023-02-19 18:48:20 +0100

    Bug fix in SessionDto.ts

--- Implementierung/frontend/paschi/src/dto/userdata/courses/SessionDto.ts
+++ Implementierung/frontend/paschi/src/dto/userdata/courses/SessionDto.ts
@@ -6,17 +6,19 @@ export class SessionDto {
   updatedAt: string;
   name: string;
   date: string;
+  interactionIds: string[]
   courseId: string;
   seatArrangementId: string;
 
   constructor(id: string, userId: string, createdAt: string, updatedAt: string, name: string, date: string,
-              courseId: string, seatArrangementId: string) {
+              intetactionIds: string[], courseId: string, seatArrangementId: string) {
     this.id = id;
     this.userId = userId;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
     this.name = name;
     this.date = date;
+    this.interactionIds = intetactionIds;
     this.courseId = courseId;
     this.seatArrangementId = seatArrangementId;
   }
