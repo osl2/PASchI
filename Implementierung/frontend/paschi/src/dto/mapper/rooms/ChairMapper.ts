fe1cb8140c5bebf53794468fc1bc2186d2f72a59
uekai <luka.kosak@student.kit.edu>
2023-02-19 19:34:20 +0100

    Implemented modelToDto() in ChairMapper.ts

--- Implementierung/frontend/paschi/src/dto/mapper/rooms/ChairMapper.ts
+++ Implementierung/frontend/paschi/src/dto/mapper/rooms/ChairMapper.ts
@@ -1,10 +1,12 @@
 import {IModelDtoMapper} from "@/dto/mapper/IModelDtoMapper";
 import {ChairDto} from "@/dto/userdata/rooms/ChairDto";
 import {Chair} from "@/model/userdata/rooms/Chair";
+import {PositionMapper} from "@/dto/mapper/rooms/PositionMapper";
 
 export class ChairMapper implements IModelDtoMapper<Chair, ChairDto> {
 
   private static mapper: ChairMapper = new ChairMapper();
+  private positionMapper = PositionMapper.getMapper();
 
   private constructor() {
   }
@@ -13,12 +15,17 @@ export class ChairMapper implements IModelDtoMapper<Chair, ChairDto> {
     return ChairMapper.mapper;
   }
 
+  modelToDto(chair: Chair): ChairDto {
+    return new ChairDto(
+      chair.getId,
+      chair.user.getId,
+      chair.createdAt,
+      chair.updatedAt,
+      this.positionMapper.modelToDto(chair.position)
+    );
+  }
+
   dtoToModel(chairDto: ChairDto): Chair {
     return undefined;
   }
-
-  modelToDto(chair: Chair): ChairDto {
-    return undefined;
-  }
-
 }
