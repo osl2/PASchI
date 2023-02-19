3a7b34b2537349abb46bae93f5aa2a006e341a55
uekai <luka.kosak@student.kit.edu>
2023-02-19 19:26:59 +0100

    Implemented RoomMapper.ts

--- Implementierung/frontend/paschi/src/dto/mapper/rooms/RoomMapper.ts
+++ Implementierung/frontend/paschi/src/dto/mapper/rooms/RoomMapper.ts
@@ -1,10 +1,14 @@
 import {IModelDtoMapper} from "@/dto/mapper/IModelDtoMapper";
 import {Room} from "@/model/userdata/rooms/Room";
 import {RoomDto} from "@/dto/userdata/rooms/RoomDto";
+import {RoomObject} from "@/model/userdata/rooms/RoomObject";
+import {RoomObjectDto} from "@/dto/userdata/rooms/RoomObjectDto";
+import {RoomObjectMapper} from "@/dto/mapper/rooms/RoomObjectMapper";
 
 export class RoomMapper implements IModelDtoMapper<Room, RoomDto> {
 
   private static mapper: RoomMapper = new RoomMapper();
+  private roomObjectMapper = RoomObjectMapper.getMapper();
 
   private constructor() {
   }
@@ -13,12 +17,23 @@ export class RoomMapper implements IModelDtoMapper<Room, RoomDto> {
     return RoomMapper.mapper;
   }
 
+  modelToDto(room: Room): RoomDto {
+    const roomObjectIds: RoomObjectDto[] = [];
+    room.roomObjects.forEach((object: RoomObject) => {
+      roomObjectIds.push(this.roomObjectMapper.modelToDto(object));
+    });
+
+    return new RoomDto(
+      room.getId,
+      room.user.getId,
+      room.createdAt,
+      room.updatedAt,
+      room.name,
+      roomObjectIds
+    );
+  }
+
   dtoToModel(roomDto: RoomDto): Room {
     return undefined;
   }
-
-  modelToDto(room: Room): RoomDto {
-    return undefined;
-  }
-
 }
