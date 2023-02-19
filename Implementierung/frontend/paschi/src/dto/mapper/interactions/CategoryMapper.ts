f89935b25a532129f42710eff981bd143a5e49a6
uekai <luka.kosak@student.kit.edu>
2023-02-19 19:02:13 +0100

    Implemented modelToDto() in CategoryMapper.ts

--- Implementierung/frontend/paschi/src/dto/mapper/interactions/CategoryMapper.ts
+++ Implementierung/frontend/paschi/src/dto/mapper/interactions/CategoryMapper.ts
@@ -1,6 +1,8 @@
 import {IModelDtoMapper} from "@/dto/mapper/IModelDtoMapper";
 import {Category} from "@/model/userdata/interactions/Category";
 import {CategoryDto} from "@/dto/userdata/interactions/CategoryDto";
+import {RatedCategoryMapper} from "@/dto/mapper/interactions/RatedCategoryMapper";
+import {RatedCategory} from "@/model/userdata/interactions/RatedCategory";
 
 export class CategoryMapper implements IModelDtoMapper<Category, CategoryDto> {
 
@@ -13,12 +15,21 @@ export class CategoryMapper implements IModelDtoMapper<Category, CategoryDto> {
     return CategoryMapper.mapper;
   }
 
+  modelToDto(category: Category): CategoryDto {
+    if (category.hasQuality()) {
+      return RatedCategoryMapper.getMapper().modelToDto(<RatedCategory> category);
+    }
+
+    return new CategoryDto(
+      category.getId,
+      category.user.getId,
+      category.createdAt,
+      category.updatedAt,
+      category.name
+    );
+  }
+
   dtoToModel(categoryDto: CategoryDto): Category {
     return undefined;
   }
-
-  modelToDto(category: Category): CategoryDto {
-    return undefined;
-  }
-
 }
