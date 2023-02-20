852d49d1c7050fad9e9e709a11cb664ff3cf33e7
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-20 17:51:51 +0100

    Removed second Parameter and Updated JavaDoc

--- Implementierung/Backend/src/main/java/edu/kit/informatik/repositories/CategoryBaseRepository.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/repositories/CategoryBaseRepository.java
@@ -1,14 +1,22 @@
 package edu.kit.informatik.repositories;
 
 import edu.kit.informatik.model.userdata.interactions.Category;
+import edu.kit.informatik.model.userdata.interactions.RatedCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Schnittstelle zur Datenbanktabelle mit {@link Category} und {@link RatedCategory}
+ * @param <CategoryType> Typ extends {@link Category}
+ *
+ * @author ugqbo
+ * @version 1.0
+ */
 @Repository
-public interface CategoryBaseRepository<CategoryType extends Category, String>
+public interface CategoryBaseRepository<CategoryType extends Category>
         extends JpaRepository<CategoryType, java.lang.String> {
 
     Optional<CategoryType> findCategoryById(java.lang.String id);
