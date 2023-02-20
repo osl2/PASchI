95e1840c1f0b23153397de92d00726b5561944d4
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-20 12:59:25 +0100

    Added Javadoc

--- Implementierung/Backend/src/main/java/edu/kit/informatik/exceptions/EntityNotFoundException.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/exceptions/EntityNotFoundException.java
@@ -1,12 +1,25 @@
 package edu.kit.informatik.exceptions;
 
+import edu.kit.informatik.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+/**
+ * Unterklasse von {@link ResponseStatusException} welche geworfen wird, falls die Entität mit der übergebenen Id
+ * nicht in der Datenbank gefunden wurde
+ *
+ * @author ugqbo
+ * @version 1.0
+ */
 public class EntityNotFoundException extends ResponseStatusException {
 
     private static final String OUTPUT = "Entity of class '%s' with id: '%s' not found";
 
+    /**
+     * Konstruktor zum Erstellen eines Objektes
+     * @param tClass Klasse in der die Exception geworfen wurde
+     * @param id Id des {@link User}
+     */
     public EntityNotFoundException(Class<?> tClass, String id) {
         super(HttpStatus.NOT_FOUND, String.format(OUTPUT, tClass.getSimpleName(), id));
     }
