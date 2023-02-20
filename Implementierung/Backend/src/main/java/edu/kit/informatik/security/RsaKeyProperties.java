2cba47e5b15653bfeb9fc5e423ae286e91d84ee5
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-20 13:26:44 +0100

    Added JavaDoc

--- Implementierung/Backend/src/main/java/edu/kit/informatik/security/RsaKeyProperties.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/security/RsaKeyProperties.java
@@ -5,6 +5,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
+/**
+ * Record für Public- und Private-Key
+ * @param publicKey {@link RSAPublicKey}
+ * @param privateKey {@link RSAPrivateKey}
+ *
+ * @author ugqbo
+ * @version 1.0
+ */
 @ConfigurationProperties(prefix = "rsa")
 public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
 
