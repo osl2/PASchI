cb373a17d8458877188c081af923e0f1a70059f4
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-20 17:55:52 +0100

    Updated JavaDoc

--- Implementierung/Backend/src/main/java/edu/kit/informatik/repositories/ChairRepository.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/repositories/ChairRepository.java
@@ -7,13 +7,17 @@ import edu.kit.informatik.model.userdata.rooms.Chair;
 import java.util.Optional;
 
 /**
- * Schnittstelle zur Datenbanktabelle mit {@link Chair Tabellen}
+ * Schnittstelle zur Datenbanktabelle mit {@link Chair Stühlen}
  *
  * @author ugqbo
  * @version 1.0
  */
 @Repository
 public interface ChairRepository extends JpaRepository<Chair, String> {
-
+    /**
+     * Rückgabe eines {@link Optional} von {@link Chair}
+     * @param id Id
+     * @return {@link Optional} von {@link Chair}
+     */
     Optional<Chair> findChairById(String id);
 }
