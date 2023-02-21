9daa8787aa188454cb5b0e17b2c6aff9ff76b17a
Florian Knechtel <fj.knechtel@gmail.com>
2023-02-21 15:47:42 +0100

    JavDoc

--- Implementierung/Backend/src/main/java/edu/kit/informatik/model/userdata/courses/Course.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/model/userdata/courses/Course.java
@@ -4,11 +4,7 @@ import edu.kit.informatik.model.DataObject;
 import edu.kit.informatik.model.User;
 import edu.kit.informatik.model.userdata.interactions.Participant;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -18,6 +14,12 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Entität eines Kurses
+ *
+ * @author ugqbo
+ * @version 1.0
+ */
 @NoArgsConstructor
 @Getter
 @Entity
@@ -45,6 +47,14 @@ public class Course extends DataObject {
     @OneToMany
     private List<SeatArrangement> seatArrangements;
 
+    /**
+     * Konstruktor zum Erstellen eines Objektes
+     * @param user {@link User}
+     * @param name Name
+     * @param subject Fach
+     * @param createdAt {@link Timestamp} der Erstellung
+     * @param updatedAt {@link Timestamp} der letzten Änderung
+     */
     public Course(User user, String name, String subject, Timestamp createdAt, Timestamp updatedAt) {
         super(user, createdAt, updatedAt);
         //this.user = user;
@@ -55,22 +65,42 @@ public class Course extends DataObject {
         this.seatArrangements = new ArrayList<>();
     }
 
+    /**
+     * Setzen des Namens
+     * @param name Name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Setzen des Faches
+     * @param subject Fach
+     */
     public void setSubject(String subject) {
         this.subject = subject;
     }
 
+    /**
+     * Setzten der {@link Participant Teilnehmer}
+     * @param participants {@link List Liste} von {@link Participant Teilnehmern}
+     */
     public void setParticipants(List<Participant> participants) {
         this.participants = participants;
     }
 
+    /**
+     * Setzen der {@link Session Sitzung}
+     * @param sessions {@link Session Sitzung}
+     */
     public void setSessions(List<Session> sessions) {
         this.sessions = sessions;
     }
 
+    /**
+     * Setzen der {@link SeatArrangement Sitzordnung}
+     * @param seatArrangements {@link SeatArrangement Sitzordnung}
+     */
     public void setSeatArrangements(List<SeatArrangement> seatArrangements) {
         this.seatArrangements = seatArrangements;
     }
