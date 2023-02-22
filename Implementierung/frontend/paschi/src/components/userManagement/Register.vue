f1a392e4983fd0cd1807ea96889e26c2cf880ec4
David <uopmx@student.kit.edu>
2023-02-22 23:26:15 +0100

    register page finished

--- Implementierung/frontend/paschi/src/components/userManagement/Register.vue
+++ Implementierung/frontend/paschi/src/components/userManagement/Register.vue
@@ -22,24 +22,45 @@
       </v-card>
       <v-card class="pa-2 rounded-lg" variant="flat" max-width="450px">
         <v-card-item>
-          <v-text-field class="mt-2" variant="outlined" label="Vorname" />
-          <v-text-field variant="outlined" label="Nachname" />
           <v-text-field
+            v-model="firstName"
+            class="mt-2"
+            variant="outlined"
+            label="Vorname"
+            :rules="[requiredRule, nameMaxLengthRule]"
+          />
+          <v-text-field
+            v-model="lastName"
+            variant="outlined"
+            label="Nachname"
+            :rules="[requiredRule, nameMaxLengthRule]"
+          />
+          <v-text-field
+            v-model="mail"
             prepend-inner-icon="mdi mdi-email-outline"
             variant="outlined"
             label="Mailadresse"
+            :rules="[requiredRule, emailRule]"
           />
           <v-text-field
+            v-model="password"
             prepend-inner-icon="mdi mdi-lock-outline"
             type="password"
             variant="outlined"
             label="Passwort"
+            :rules="[
+              requiredRule,
+              passwordMinLengthRule,
+              passwordMaxLengthRule,
+            ]"
           />
           <v-text-field
+            v-model="passwordRepeat"
             prepend-inner-icon="mdi mdi-lock-outline"
             type="password"
             variant="outlined"
             label="Passwort bestätigen"
+            :rules="[requiredRule, passwordsEqualRule]"
           />
         </v-card-item>
         <v-card-item>
@@ -49,6 +70,7 @@
             variant="flat"
             prepend-icon="fas fa-sign-in-alt"
             color="primary"
+            @click="register()"
             >Registrieren</v-btn
           >
         </v-card-item>
@@ -69,17 +91,109 @@
 </template>
 
 <script lang="ts">
-import { useUserStore } from "@/store/UserStore";
 import AppBar from "@/components/navigation/NavigationBar.vue";
 import router from "@/plugins/router";
+import { ref } from "vue";
+import { UserController } from "@/controller/UserController";
 export default {
   name: "Register",
   components: { AppBar },
+
   setup() {
-    const userStore = useUserStore();
+    const firstName = ref("");
+    const lastName = ref("");
+    const mail = ref("");
+    const password = ref("");
+    const passwordRepeat = ref("");
+    const userController = UserController.getUserController();
+    function requiredRule(value: string) {
+      if (value === "") {
+        return "Dieses Feld muss ausgefüllt sein.";
+      }
+      return true;
+    }
+    function emailRule() {
+      if (!mail.value.match("(.+)@(.+)\\.(.+)") || mail.value.length > 254) {
+        return "Keine gültige Email Adresse.";
+      }
+      return true;
+    }
+    function passwordMinLengthRule() {
+      if (password.value.length < 8) {
+        return "Das Passwort muss mindestens 8 Zeichen lang sein.";
+      }
+      return true;
+    }
+    function passwordMaxLengthRule() {
+      if (password.value.length > 32) {
+        return "Das Passwort muss höchstens 32 Zeichen lang sein.";
+      }
+      return true;
+    }
+    function passwordsEqualRule() {
+      if (password.value !== passwordRepeat.value) {
+        return "Passwörter müssen gleich sein.";
+      }
+      return true;
+    }
+    function nameMaxLengthRule(value: string) {
+      if (value.length > 20) {
+        return "Namen müssen kürzer als 20 Zeichen sein";
+      }
+      return true;
+    }
+    function error(ruleReturns: (boolean | string)[]): boolean {
+      for (let i = 0; i < ruleReturns.length; i++) {
+        if (typeof ruleReturns[i] === "string" || !ruleReturns[i]) {
+          return true;
+        }
+      }
+      return false;
+    }
+    function register() {
+      if (
+        !error([
+          requiredRule(firstName.value),
+          requiredRule(lastName.value),
+          requiredRule(mail.value),
+          requiredRule(password.value),
+          requiredRule(passwordRepeat.value),
+          emailRule(),
+          nameMaxLengthRule(firstName.value),
+          nameMaxLengthRule(lastName.value),
+          passwordMinLengthRule(),
+          passwordMaxLengthRule(),
+          passwordsEqualRule(),
+        ])
+      ) {
+        userController.register(
+          firstName.value,
+          lastName.value,
+          mail.value,
+          password.value,
+          passwordRepeat.value
+        );
+        router.push("login");
+      }
+      else {
+        firstName.value += "a"
+        firstName.value = ""
+      }
+    }
 
     return {
-      userStore,
+      firstName,
+      lastName,
+      mail,
+      password,
+      passwordRepeat,
+      register,
+      emailRule,
+      requiredRule,
+      passwordMinLengthRule,
+      passwordMaxLengthRule,
+      passwordsEqualRule,
+      nameMaxLengthRule,
       router,
     };
   },
