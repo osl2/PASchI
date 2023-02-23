d692f6962793db2ce4e1d2224641bb7e43ec42f2
uekai <luka.kosak@student.kit.edu>
2023-02-23 15:06:50 +0100

    Implemented RoomMapper.ts

--- Implementierung/frontend/paschi/src/dto/mapper/rooms/RoomMapper.ts
+++ Implementierung/frontend/paschi/src/dto/mapper/rooms/RoomMapper.ts
@@ -4,6 +4,8 @@ import {RoomDto} from "@/dto/userdata/rooms/RoomDto";
 import {RoomObject} from "@/model/userdata/rooms/RoomObject";
 import {RoomObjectDto} from "@/dto/userdata/rooms/RoomObjectDto";
 import {RoomObjectMapper} from "@/dto/mapper/rooms/RoomObjectMapper";
+import {useRoomStore} from "@/store/RoomStore";
+import {UserController} from "@/controller/UserController";
 
 export class RoomMapper implements IModelDtoMapper<Room, RoomDto> {
 
@@ -34,6 +36,31 @@ export class RoomMapper implements IModelDtoMapper<Room, RoomDto> {
   }
 
   async dtoToModel(roomDto: RoomDto): Promise<Room> {
-    return undefined;
+    const userController = UserController.getUserController();
+
+    let room = useRoomStore().getRoom(roomDto.id);
+    if (room == undefined) {
+      room = new Room(
+        roomDto.id,
+        0,
+        userController.getUser(),
+        roomDto.name
+      );
+      room.createdAt = roomDto.createdAt;
+      room.updatedAt = roomDto.updatedAt;
+      useRoomStore().addRoom(room);
+    } else if (room.updatedAt === roomDto.updatedAt) {
+      return room;
+    } else {
+      room.name = roomDto.name;
+    }
+
+    const roomObjects: RoomObject[] = [];
+    for (const object of roomDto.roomObjectDtos) {
+      roomObjects.push(await this.roomObjectMapper.dtoToModel(object));
+    }
+
+    room.roomObjects = roomObjects;
+    return room;
   }
 }
