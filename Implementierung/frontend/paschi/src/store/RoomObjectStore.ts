7b96811c2def1ca39b9815f893257a024158a1e3
uekai <luka.kosak@student.kit.edu>
2023-02-23 15:19:03 +0100

    Edited RoomObjectStore.ts

--- Implementierung/frontend/paschi/src/store/RoomObjectStore.ts
+++ Implementierung/frontend/paschi/src/store/RoomObjectStore.ts
@@ -1,10 +1,36 @@
 import {defineStore} from "pinia";
+import {Chair} from "@/model/userdata/rooms/Chair";
+import {Table} from "@/model/userdata/rooms/Table";
 
 export const useRoomObjectStore = defineStore('roomObjects', {
   state: () => ({
+    chairs: [] as Chair[],
+    tables: [] as Table[],
     nextId: 0
   }),
   actions: {
+    addChair(chair: Chair) {
+      this.chairs.push(chair);
+    },
+    addTable(table: Table) {
+      this.tables.push(table);
+    },
+    getChair(id: string): Chair | undefined {
+      for (const chair of this.chairs) {
+        if (chair.getId === id) {
+          return <Chair> chair;
+        }
+      }
+      return undefined;
+    },
+    getTable(id: string): Table | undefined {
+      for (const table of this.tables) {
+        if (table.getId === id) {
+          return <Table> table;
+        }
+      }
+      return undefined;
+    },
     getNextId(): number {
       return this.nextId++;
     }
