75ce4d6a720c2b7e36e11597dcf80869d228cd5f
uekai <luka.kosak@student.kit.edu>
2023-02-24 17:31:55 +0100

    Added jsdoc to DataObject.ts

--- Implementierung/frontend/paschi/src/model/DataObject.ts
+++ Implementierung/frontend/paschi/src/model/DataObject.ts
@@ -1,3 +1,9 @@
+/**
+ * Abstraktes Datenobjekt.
+ *
+ * @author uekai
+ * @version 1.0
+ */
 export abstract class DataObject {
 
   private _id: string | undefined;
@@ -13,15 +19,24 @@ export abstract class DataObject {
     this._updatedAt = date.toString();
   }
 
+  /**
+   * Aktualisiert den Timestamp des Objekts.
+   */
   update() {
     const date = new Date().toISOString();
     this._updatedAt = date.toString();
   }
 
+  /**
+   * Gibt zurück, ob das Objekt eine gültige ID besitzt.
+   */
   hasId(): boolean {
     return this._id != undefined;
   }
 
+  /**
+   * Gibt die ID des Objekts zurück. Falls keine gültige ID existiert, wird die lokale ID zurückgegeben.
+   */
   get getId(): string {
     if (this._id == undefined) {
       return this._localId.toString();
@@ -29,22 +44,47 @@ export abstract class DataObject {
     return this._id;
   }
 
+  /**
+   * Setzt die ID des Objekts.
+   *
+   * @param id Die ID des Objekts.
+   */
   set setId(id: string) {
     this._id = id;
   }
 
+  /**
+   * Setzt den createdAt Timestamp.
+   *
+   * @param value Der createdAt Timestamp.
+   */
   set createdAt(value: string) {
     this._createdAt = value;
   }
 
+  /**
+   * Setzt den updatedAt Timestamp.
+   *
+   * @param value Der updatedAt Timestamp.
+   */
   set updatedAt(value: string) {
     this._updatedAt = value;
   }
 
+  /**
+   * Gibt den createdAt Timestamp zurück.
+   *
+   * @return Der createdAt Timestamp.
+   */
   get createdAt(): string {
     return this._createdAt;
   }
 
+  /**
+   * Gibt den updatedAtTimestamp zurück.
+   *
+   * @return Der updatedAt Timestamp.
+   */
   get updatedAt(): string {
     return this._updatedAt;
   }
