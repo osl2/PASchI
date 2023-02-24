903df488e23b51081ce9c7b9c3cff21824ccb294
Cian Payne <cian.payne@student.kit.edu>
2023-02-24 18:47:10 +0100

    Prevent text selection of labels

--- Implementierung/frontend/paschi/src/components/room/SeatLabel.vue
+++ Implementierung/frontend/paschi/src/components/room/SeatLabel.vue
@@ -3,13 +3,13 @@
     v-if="participant"
     class="flex-column align-content-center justify-center"
   >
-    <div class="d-flex align center justify-center">
+    <div class="d-flex align center justify-center" style="user-select: none; -webkit-user-select: none">
       <v-avatar class="font-weight-medium text-lg-button" color="primary">
         {{ participant.firstName.charAt(0) + " " + participant.lastName.charAt(0) }}
         <slot />
       </v-avatar>
     </div>
-    <div class="col text-center font-weight-medium">
+    <div class="col text-center font-weight-medium" style="user-select: none; -webkit-user-select: none">
       {{ participant.firstName }}
       {{ participant.lastName }}
     </div>
