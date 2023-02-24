7845f95e30e27bbfd8a3d253ac9453776ebb755b
uekai <luka.kosak@student.kit.edu>
2023-02-24 23:05:48 +0100

    Added jsdoc in StudentController.ts

--- Implementierung/frontend/paschi/src/controller/StudentController.ts
+++ Implementierung/frontend/paschi/src/controller/StudentController.ts
@@ -21,6 +21,12 @@ export class StudentController {
     return this.controller;
   }
 
+  /**
+   * Erstellt einen neuen Schüler.
+   *
+   * @param firstName Vorname
+   * @param lastName Nachname
+   */
   async createStudent(firstName: string, lastName: string): Promise<string> {
     const student = new Student(
       undefined,
@@ -33,6 +39,13 @@ export class StudentController {
     return useStudentStore().addStudent(student);
   }
 
+  /**
+   * Aktualisiert die Daten eines Schüler.
+   *
+   * @param id ID des Schülers
+   * @param firstName Vorname
+   * @param lastName Nachname
+   */
   async updateStudent(id: string, firstName: string, lastName: string) {
     let student = useStudentStore().getStudent(id);
     if (student !== undefined) {
@@ -42,6 +55,11 @@ export class StudentController {
     }
   }
 
+  /**
+   * Löscht einen Schüler.
+   *
+   * @param id ID des Schülers
+   */
   async deleteStudent(id: string) {
     let student = useStudentStore().getStudent(id);
     if (student !== undefined) {
@@ -53,10 +71,18 @@ export class StudentController {
     }
   }
 
+  /**
+   * Gibt den Schüler mit der übergebenen ID zurück.
+   *
+   * @param id ID des Schülers
+   */
   getStudent(id: string): Student | undefined {
     return useStudentStore().getStudent(id);
   }
 
+  /**
+   * Gibt alle Schüler des Benutzers zurück.
+   */
   getAllStudents(): Student[] {
     this.studentService.getAll().then();
     return useStudentStore().getAllStudents();
