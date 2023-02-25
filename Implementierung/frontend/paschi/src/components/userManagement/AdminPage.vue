1322052595cb2b27d4a4922fdadec50c7dea3e1a
Cián Payne <ciandanielpayne@gmail.com>
2023-02-25 15:57:27 +0100

    Add logout to AdminPage

--- Implementierung/frontend/paschi/src/components/userManagement/AdminPage.vue
+++ Implementierung/frontend/paschi/src/components/userManagement/AdminPage.vue
@@ -1,12 +1,33 @@
 <template>
+  <NavigationBar>
+    <template v-slot:prepend>
+      <div />
+    </template>
+    <template #default>
+      <v-app-bar-title>Administratorseite</v-app-bar-title>
+    </template>
+    <template #append>
+      <v-btn variant="tonal" @click="logOut" color="white" rounded="pill">
+        Abmelden
+      </v-btn>
+    </template>
+  </NavigationBar>
   <v-main>
     <v-container fluid>
       <v-row justify="space-around" align-content="stretch">
         <v-col>
-          <RequestListCard :requests="requests" @update-requests="updateUsers" class="ma-3" />
+          <RequestListCard
+            :requests="requests"
+            @update-requests="updateUsers"
+            class="ma-3"
+          />
         </v-col>
         <v-col>
-          <UserListCard :users="users" @update-users="updateUsers" class="ma-3" />
+          <UserListCard
+            :users="users"
+            @update-users="updateUsers"
+            class="ma-3"
+          />
         </v-col>
       </v-row>
     </v-container>
@@ -18,23 +39,34 @@ import AppBar from "@/components/navigation/NavigationBar.vue";
 import router from "@/plugins/router";
 import UserListCard from "@/components/userManagement/UserListCard.vue";
 import RequestListCard from "@/components/userManagement/RequestListCard.vue";
-import {AdminController} from "@/controller/AdminController";
-import {onBeforeMount, ref} from "vue";
-import {User} from "@/model/User";
+import NavigationBar from "@/components/navigation/NavigationBar.vue";
+import { AdminController } from "@/controller/AdminController";
+import { onBeforeMount, ref } from "vue";
+import { User } from "@/model/User";
+import { UserController } from "@/controller/UserController";
 export default {
   name: "AdminPage",
-  components: { AppBar, RequestListCard, UserListCard },
+  components: { AppBar, NavigationBar, RequestListCard, UserListCard },
   setup() {
     const adminController = AdminController.getAdminController();
+    const userController = UserController.getUserController();
 
     const users = ref<User[]>([]);
     const requests = ref<User[]>([]);
 
+    /**
+     * Methode zum Ausloggen.
+     */
+    function logOut() {
+      userController.logout();
+      router.push("/login");
+    }
 
     async function updateUsers() {
-      await adminController.getUsersNotAuthenticated().then((res) => requests.value = res);
-      await adminController.getUsers().then((res) => users.value = res);
-
+      await adminController
+        .getUsersNotAuthenticated()
+        .then((res) => (requests.value = res));
+      await adminController.getUsers().then((res) => (users.value = res));
     }
 
     onBeforeMount(() => {
@@ -44,6 +76,7 @@ export default {
     return {
       router,
       updateUsers,
+      logOut,
       users,
       requests,
     };
