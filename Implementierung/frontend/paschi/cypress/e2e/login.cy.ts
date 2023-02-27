578333d6f731a9d46603dcb76499497f45f8d95e
Cián Payne <ciandanielpayne@gmail.com>
2023-02-28 00:15:21 +0100

    Added login test case

---
+++ Implementierung/frontend/paschi/cypress/e2e/login.cy.ts
@@ -0,0 +1,18 @@
+///<reference types="cypress" />
+const user = { email: "admin@kit.edu", password: "admin" };
+
+describe("Login test", () => {
+  it("tests input of email and password and successful login", () => {
+    cy.visit("/login");
+    cy.get("input[name='email']").type(user.email);
+    cy.get("input[name='password']").type(user.password);
+    cy.get("button[type=submit]").click();
+    cy.url().should("include", "/admin");
+    cy.contains("Akzeptierte Accounts");
+    cy.contains("Accountanträge");
+    cy.contains("Administratorseite");
+    cy.contains("Abmelden");
+    cy.get("button").contains("Abmelden").click();
+    cy.url().should("include", "/login");
+  });
+});
