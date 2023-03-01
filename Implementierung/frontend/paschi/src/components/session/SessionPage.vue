9a010607400403ce88dce66763e1d66869821e5c
David <uopmx@student.kit.edu>
2023-03-01 19:38:37 +0100

    SessionPage interaction quality updated

--- Implementierung/frontend/paschi/src/components/session/SessionPage.vue
+++ Implementierung/frontend/paschi/src/components/session/SessionPage.vue
@@ -221,11 +221,11 @@
               <template v-if="interaction.category.hasQuality()">
                 <v-icon
                   icon="mdi mdi-star"
-                  v-for="i in interaction.category.getQuality() + 1"
+                  v-for="i in getStars(interaction.category.getQuality())"
                 ></v-icon>
                 <v-icon
                   icon="mdi mdi-star-outline"
-                  v-for="i in 5 - (interaction.category.getQuality() + 1)"
+                  v-for="i in 5 - (getStars(interaction.category.getQuality()))"
                 ></v-icon>
               </template>
             </v-col>
@@ -283,11 +283,11 @@
               <template v-if="interaction.category.hasQuality()">
                 <v-icon
                   icon="mdi mdi-star"
-                  v-for="i in interaction.category.getQuality() + 1"
+                  v-for="i in getStars(interaction.category.getQuality())"
                 ></v-icon>
                 <v-icon
                   icon="mdi mdi-star-outline"
-                  v-for="i in 5 - (interaction.category.getQuality() + 1)"
+                  v-for="i in 5 - (getStars(interaction.category.getQuality()))"
                 ></v-icon>
               </template>
             </v-col>
@@ -620,6 +620,23 @@ export default defineComponent({
       interactionListDialog.value = true;
     }
 
+    function getStars(quality: Quality) {
+      switch (quality) {
+        case Quality.ONE_STAR:
+          return 1;
+        case Quality.TWO_STAR:
+          return 2;
+        case Quality.THREE_STAR:
+          return 3;
+        case Quality.FOUR_STAR:
+          return 4;
+        case Quality.FIVE_STAR:
+          return 5;
+        default:
+          return 0
+      }
+    }
+
     return {
       starDialog,
       teacher,
@@ -652,6 +669,7 @@ export default defineComponent({
       confirmAddCategory,
       cancelAddCategory,
       activateStudentInteractionList,
+      getStars
     };
   },
 });
