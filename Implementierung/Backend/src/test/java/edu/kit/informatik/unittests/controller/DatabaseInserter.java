44244c1e23cd5663e883fd8c0ed2eb18346b68a7
Florian Knechtel <fj.knechtel@gmail.com>
2023-03-08 19:32:59 +0100

    Added method to Put User in Database

--- Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/DatabaseInserter.java
+++ Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/DatabaseInserter.java
@@ -1,4 +1,23 @@
 package edu.kit.informatik.unittests.controller;
 
-public class DatabaseInserter {
+import edu.kit.informatik.dto.UserDto;
+import edu.kit.informatik.dto.mapper.UserMapper;
+import edu.kit.informatik.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class DatabaseInserter {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    UserMapper userMapper;
+
+    public UserDto addUserToDatabase(UserDto userDto) {
+        userDto.setPassword("{bcrypt}" + new BCryptPasswordEncoder().encode(userDto.getPassword()));
+        return userMapper.modelToDto(this.userRepository.save(userMapper.dtoToModel(userDto)));
+    }
+
 }
