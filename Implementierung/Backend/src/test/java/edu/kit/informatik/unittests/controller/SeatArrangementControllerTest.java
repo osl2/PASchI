97619e7e67509687ee44ad35f0747ae63de062f0
Florian Knechtel <fj.knechtel@gmail.com>
2023-03-10 18:11:09 +0100

    Outsource Methods

--- Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/SeatArrangementControllerTest.java
+++ Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/SeatArrangementControllerTest.java
@@ -2,22 +2,14 @@ package edu.kit.informatik.unittests.controller;
 
 
 import com.github.javafaker.Faker;
-import edu.kit.informatik.dto.RoleDto;
 import edu.kit.informatik.dto.UserDto;
-import edu.kit.informatik.dto.mapper.UserMapper;
-import edu.kit.informatik.dto.mapper.courses.CourseMapper;
 import edu.kit.informatik.dto.mapper.courses.SeatArrangementMapper;
-import edu.kit.informatik.dto.mapper.rooms.RoomMapper;
 import edu.kit.informatik.dto.userdata.courses.CourseDto;
 import edu.kit.informatik.dto.userdata.courses.SeatArrangementDto;
 import edu.kit.informatik.dto.userdata.rooms.RoomDto;
-import edu.kit.informatik.model.User;
-import edu.kit.informatik.model.userdata.courses.Course;
-import edu.kit.informatik.model.userdata.rooms.Room;
-import edu.kit.informatik.repositories.CourseRepository;
-import edu.kit.informatik.repositories.RoomRepository;
 import edu.kit.informatik.repositories.SeatArrangementRepository;
-import edu.kit.informatik.repositories.UserRepository;
+import edu.kit.informatik.unittests.DatabaseManipulator;
+import edu.kit.informatik.unittests.EntityGenerator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -27,13 +19,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.nio.charset.StandardCharsets;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -45,46 +33,37 @@ public class SeatArrangementControllerTest extends AbstractTest {
     private static final String BASE_URL = "/api/seatarrangement";
 
     @Autowired
+    private DatabaseManipulator databaseManipulator;
+    @Autowired
     private SeatArrangementMapper seatArrangementMapper;
 
     @Autowired
     private SeatArrangementRepository seatArrangementRepository;
 
-    @Autowired
-    private CourseRepository courseRepository;
-
-    @Autowired
-    private CourseMapper courseMapper;
-
-    @Autowired
-    private RoomRepository roomRepository;
-
-    @Autowired
-    private RoomMapper roomMapper;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserMapper userMapper;
-
-
     private List<SeatArrangementDto> seatArrangements;
+    private UserDto userDto;
+    private RoomDto roomDto;
+    private CourseDto courseDto;
 
     @Before
     @Override
-    public void setUp() {
+    public void setUp() throws Exception {
         super.setUp();
+        Faker faker = new Faker(new Locale("de"));
+        userDto = super.addAndLogin(EntityGenerator.createNewUser(faker));
+        courseDto = databaseManipulator.addCourse(EntityGenerator.createNewCourse(faker, userDto));
+        roomDto = databaseManipulator.addRoom(EntityGenerator.createNewRoom(faker, userDto));
         this.seatArrangements = addSomeSeatArrangements();
     }
 
     @After
     @Override
     public void setDown() {
-        this.courseRepository.deleteAll();
-        this.seatArrangementRepository.deleteAll();
-        this.roomRepository.deleteAll();
-        this.userRepository.deleteAll();
+        databaseManipulator.clearCourseRepository();
+        databaseManipulator.clearSeatArrangementRepository();
+        databaseManipulator.clearRoomRepository();
+        databaseManipulator.clearUserRepository();
+        this.seatArrangements.clear();
     }
 
     private List<SeatArrangementDto> addSomeSeatArrangements() {
@@ -93,22 +72,11 @@ public class SeatArrangementControllerTest extends AbstractTest {
         Faker faker = new Faker(new Locale("de"));
 
         for (int i = 0; i < 10; i++) {
-            seatArrangementDtos.add(getNewSeatArrangement(faker));
+            seatArrangementDtos.add(EntityGenerator.createNewSeatArrangement(faker, userDto, courseDto, roomDto));
         }
 
         return seatArrangementDtos;
 
-    }
-
-    private void deleteFromDataBase() {
-
-        for (SeatArrangementDto seatArrangementDto: seatArrangements) {
-            if (seatArrangementDto.getId() != null) {
-                this.seatArrangementRepository.deleteById(seatArrangementDto.getId());
-            }
-        }
-
-        seatArrangements.clear();
     }
 
     private void addSeatArrangementToDatabase() {
@@ -149,7 +117,6 @@ public class SeatArrangementControllerTest extends AbstractTest {
             assertNotNull(seatArrangementDtoFromContent.getId());
             seatArrangements.set(seatArrangements.indexOf(seatArrangementDto), seatArrangementDtoFromContent);
         }
-        deleteFromDataBase();
     }
 
 
@@ -170,8 +137,6 @@ public class SeatArrangementControllerTest extends AbstractTest {
 
             assertEquals(seatArrangementDto, seatArrangementDtoFromDatabase);
         }
-
-        deleteFromDataBase();
     }
 
     @Test
@@ -194,13 +159,11 @@ public class SeatArrangementControllerTest extends AbstractTest {
         for (int i= 0; i < seatArrangements.size(); i++) {
             assertEquals(seatArrangements.get(i), seatArrangementDtos.get(i));
         }
-
-        deleteFromDataBase();
     }
 
     @Test
     public void deleteSeatArrangements() throws Exception {
-        List<SeatArrangementDto> before = getSeatArrangementsFromDataBase();
+        List<SeatArrangementDto> before = databaseManipulator.getSeatArrangements();
         addSeatArrangementToDatabase();
 
         for (SeatArrangementDto seatArrangementDto: seatArrangements) {
@@ -210,7 +173,7 @@ public class SeatArrangementControllerTest extends AbstractTest {
             int status = mvcResult.getResponse().getStatus();
             assertEquals(200, status);
         }
-        List<SeatArrangementDto> after = getSeatArrangementsFromDataBase();
+        List<SeatArrangementDto> after = databaseManipulator.getSeatArrangements();
 
         assertEquals(before.size(), after.size());
 
@@ -218,74 +181,5 @@ public class SeatArrangementControllerTest extends AbstractTest {
             assertEquals(before.get(i), after.get(i));
         }
 
-    }
-
-    private List<SeatArrangementDto> getSeatArrangementsFromDataBase() {
-        return seatArrangementMapper.modelToDto(this.seatArrangementRepository.findAll());
-    }
-
-
-    private SeatArrangementDto getNewSeatArrangement(Faker faker) {
-
-        SeatArrangementDto seatArrangementDto = new SeatArrangementDto();
-
-        String name = faker.team().name();
-
-        seatArrangementDto.setName(name + " " + faker.number().randomDigit());
-
-        User user = userRepository.save(userMapper.dtoToModel(getNewUser(faker)));
-
-        seatArrangementDto.setUserId(user.getId());
-        seatArrangementDto.setSeatMap(new HashMap<>());
-
-        Course course = courseRepository.save(courseMapper.dtoToModel(getNewCourse(faker, user.getId())));
-        seatArrangementDto.setCourseId(course.getId());
-
-        Room room = roomRepository.save(roomMapper.dtoToModel(getNewRoom(faker, user.getId())));
-        seatArrangementDto.setRoomId(room.getId());
-        seatArrangementDto.setCreatedAt(Timestamp.from(Instant.now().truncatedTo(ChronoUnit.SECONDS)));
-
-        return seatArrangementDto;
-    }
-
-    private CourseDto getNewCourse(Faker faker, String userId) {
-        CourseDto courseDto = new CourseDto();
-
-        String name = faker.team().name();
-
-        courseDto.setName(name + " " + faker.number().randomDigit());
-        courseDto.setSubject(name);
-        courseDto.setUserId(userId);
-        courseDto.setCreatedAt(Timestamp.from(Instant.now().truncatedTo(ChronoUnit.SECONDS)));
-
-        return courseDto;
-    }
-
-    private RoomDto getNewRoom(Faker faker, String userId) {
-        RoomDto roomDto = new RoomDto();
-
-        roomDto.setName(String.valueOf(faker.number().randomDigit()));
-
-        roomDto.setUserId(userId);
-        roomDto.setRoomObjects(new ArrayList<>());
-        roomDto.setCreatedAt(Timestamp.from(Instant.now().truncatedTo(ChronoUnit.SECONDS)));
-
-        return roomDto;
-    }
-
-    private UserDto getNewUser(Faker faker) {
-        UserDto userDto = new UserDto();
-        userDto.setRole(RoleDto.USER);
-
-        String firstName = faker.name().firstName();
-        String lastName = faker.name().lastName();
-
-        userDto.setEmail(firstName + "." + lastName +  "@kit.edu");
-        userDto.setFirstName(firstName);
-        userDto.setLastName(lastName);
-        userDto.setPassword(faker.crypto().md5());
-        userDto.setCreatedAt(Timestamp.from(Instant.now().truncatedTo(ChronoUnit.SECONDS)));
-
-        return userDto;
     }
 }
