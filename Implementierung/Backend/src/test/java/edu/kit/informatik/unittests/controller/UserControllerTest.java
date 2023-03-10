9b852270b50b4d9851bcd75ec00d17c8ccfed098
Florian Knechtel <fj.knechtel@gmail.com>
2023-03-10 19:09:45 +0100

    Added Authorization && adminUpdateTest-Method

--- Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/UserControllerTest.java
+++ Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package edu.kit.informatik.unittests.controller;
 
 import com.github.javafaker.Faker;
+import edu.kit.informatik.dto.RoleDto;
 import edu.kit.informatik.dto.UserDto;
 import edu.kit.informatik.unittests.DatabaseManipulator;
 import edu.kit.informatik.unittests.EntityGenerator;
@@ -19,8 +20,10 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -33,10 +36,13 @@ public class UserControllerTest extends AbstractTest {
 
     private List<UserDto> users;
 
+    private UserDto mainUserDto;
+
     @Before
     @Override
     public void setUp() throws Exception {
         super.setUp();
+        mainUserDto = super.addAndLogin(EntityGenerator.createNewAdmin(new Faker(new Locale("de"))));
         this.users = getNewUsers();
     }
 
@@ -99,7 +105,8 @@ public class UserControllerTest extends AbstractTest {
 
         for (int i = 0; i< users.size(); i++) {
             MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(BASE_URL).contentType(MediaType.APPLICATION_JSON)
-                            .content(super.mapToJson(users.get(i)))
+                    .content(super.mapToJson(users.get(i)))
+                    .header("Authorization", "Bearer " + mainUserDto.getToken())
             ).andReturn();
 
             int status = mvcResult.getResponse().getStatus();
@@ -135,7 +142,8 @@ public class UserControllerTest extends AbstractTest {
         for (UserDto userDto: users) {
 
             MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(BASE_URL).contentType(MediaType.APPLICATION_JSON)
-                            .content(super.mapToJson(userDto))
+                        .content(super.mapToJson(userDto))
+                    .header("Authorization", "Bearer " + mainUserDto.getToken())
             ).andReturn();
 
             int status = mvcResult.getResponse().getStatus();
@@ -143,6 +151,12 @@ public class UserControllerTest extends AbstractTest {
         }
 
         List<UserDto> userFromDataBase = databaseManipulator.getUsers();
+        for (UserDto userDto: userFromDataBase) {
+            if (Objects.equals(userDto.getId(), mainUserDto.getId())) {
+                userFromDataBase.remove(userDto);
+                break;
+            }
+        }
 
         userFromDataBase.sort(Comparator.naturalOrder());
         users.sort(Comparator.naturalOrder());
@@ -155,11 +169,59 @@ public class UserControllerTest extends AbstractTest {
     }
 
     @Test
+    public void adminUpdateUser() throws Exception {
+        addUsersToDatabase();
+
+        List<UserDto> newUsers = getNewUsers();
+
+        for (int i = 0; i < users.size(); i++) {
+            users.get(i).setFirstName(newUsers.get(i).getFirstName());
+            users.get(i).setLastName(newUsers.get(i).getLastName());
+            users.get(i).setRole(RoleDto.ADMIN);
+            users.get(i).setPassword(newUsers.get(i).getPassword());
+            users.get(i).setAuth(false);
+        }
+
+        for (UserDto userDto: users) {
+
+            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(BASE_URL + "/admin").contentType(MediaType.APPLICATION_JSON)
+                    .content(super.mapToJson(userDto))
+                    .header("Authorization", "Bearer " + mainUserDto.getToken())
+            ).andReturn();
+
+            int status = mvcResult.getResponse().getStatus();
+            assertEquals(200, status);
+        }
+
+        List<UserDto> userFromDataBase = databaseManipulator.getUsers();
+        for (UserDto userDto: userFromDataBase) {
+            if (Objects.equals(userDto.getId(), mainUserDto.getId())) {
+                userFromDataBase.remove(userDto);
+                break;
+            }
+        }
+
+        userFromDataBase.sort(Comparator.naturalOrder());
+        users.sort(Comparator.naturalOrder());
+
+        assertEquals(userFromDataBase.size(), users.size());
+
+        for (int i = 0; i < users.size(); i++) {
+            assertNotEquals(users.get(i).getFirstName(), userFromDataBase.get(i).getFirstName());
+            assertNotEquals(users.get(i).getLastName(), userFromDataBase.get(i).getLastName());
+            assertNotEquals(users.get(i).getPassword(), userFromDataBase.get(i).getPassword());
+            assertEquals(users.get(i).getRole(), userFromDataBase.get(i).getRole());
+            assertEquals(users.get(i).isAuth(), userFromDataBase.get(i).isAuth());
+        }
+    }
+
+    @Test
     public void getOneUser() throws Exception {
         addUsersToDatabase();
 
         for (UserDto user: users) {
             MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/" + user.getId())
+                    .header("Authorization", "Bearer " + mainUserDto.getToken())
             ).andReturn();
 
             int status = mvcResult.getResponse().getStatus();
@@ -169,7 +231,12 @@ public class UserControllerTest extends AbstractTest {
             UserDto userdto = super.mapFromJson(content, UserDto.class);
 
 
-            assertEquals(user, userdto);
+            assertEquals(user.getEmail(), userdto.getEmail());
+            userdto.setPassword(userdto.getPassword().substring(8));
+            assertTrue(new BCryptPasswordEncoder().matches(user.getPassword(), userdto.getPassword()));
+            assertEquals(user.getFirstName(), userdto.getFirstName());
+            assertEquals(user.getLastName(), userdto.getLastName());
+            assertEquals(user.getRole(), userdto.getRole());
         }
 
     }
@@ -179,6 +246,7 @@ public class UserControllerTest extends AbstractTest {
         addUsersToDatabase();
 
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/admin")
+                .header("Authorization", "Bearer " + mainUserDto.getToken())
         ).andReturn();
 
         int status = mvcResult.getResponse().getStatus();
@@ -186,13 +254,20 @@ public class UserControllerTest extends AbstractTest {
         String content = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
 
         List<UserDto> userDtos = Arrays.asList(super.mapFromJson(content, UserDto[].class));
+        users.add(this.mainUserDto);
 
         userDtos.sort(Comparator.naturalOrder());
         users.sort(Comparator.naturalOrder());
 
 
+
         for (int i= 0; i < users.size(); i++) {
-            assertEquals(users.get(i), userDtos.get(i));
+            assertEquals(users.get(i).getEmail(), userDtos.get(i).getEmail());
+            userDtos.get(i).setPassword(userDtos.get(i).getPassword().substring(8));
+            assertTrue(new BCryptPasswordEncoder().matches(users.get(i).getPassword(), userDtos.get(i).getPassword()));
+            assertEquals(users.get(i).getFirstName(), userDtos.get(i).getFirstName());
+            assertEquals(users.get(i).getLastName(), userDtos.get(i).getLastName());
+            assertEquals(users.get(i).getRole(), userDtos.get(i).getRole());
         }
 
     }
@@ -203,7 +278,9 @@ public class UserControllerTest extends AbstractTest {
         addUsersToDatabase();
 
         for (UserDto user: users) {
-            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(BASE_URL).content(user.getId()).param("id", user.getId())
+            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(BASE_URL).content(user.getId())
+                    .param("id", user.getId())
+                    .header("Authorization", "Bearer " + mainUserDto.getToken())
             ).andReturn();
 
             int status = mvcResult.getResponse().getStatus();
