cf95cccbe47c2d261ff9058b768f44675ab29f33
Florian Knechtel <fj.knechtel@gmail.com>
2023-03-10 18:01:17 +0100

    Outsource Methods

--- Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/RoomControllerTest.java
+++ Implementierung/Backend/src/test/java/edu/kit/informatik/unittests/controller/RoomControllerTest.java
@@ -1,14 +1,10 @@
 package edu.kit.informatik.unittests.controller;
 
 import com.github.javafaker.Faker;
-import edu.kit.informatik.dto.RoleDto;
 import edu.kit.informatik.dto.UserDto;
-import edu.kit.informatik.dto.mapper.UserMapper;
-import edu.kit.informatik.dto.mapper.rooms.RoomMapper;
 import edu.kit.informatik.dto.userdata.rooms.RoomDto;
-import edu.kit.informatik.model.User;
-import edu.kit.informatik.repositories.RoomRepository;
-import edu.kit.informatik.repositories.UserRepository;
+import edu.kit.informatik.unittests.DatabaseManipulator;
+import edu.kit.informatik.unittests.EntityGenerator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,9 +14,6 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.nio.charset.StandardCharsets;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -33,33 +26,26 @@ public class RoomControllerTest extends AbstractTest {
 
     private static final String BASE_URL = "/api/room";
 
-
     @Autowired
-    private RoomMapper roomMapper;
-
-    @Autowired
-    private RoomRepository roomRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserMapper userMapper;
-
+    DatabaseManipulator databaseManipulator;
 
     private List<RoomDto> rooms;
+    private UserDto userDto;
 
     @Before
     @Override
-    public void setUp() {
+    public void setUp() throws Exception {
         super.setUp();
+        userDto = super.addAndLogin(EntityGenerator.createNewUser(new Faker(new Locale("de"))));
         this.rooms = addSomeRooms();
     }
 
     @After
     @Override
     public void setDown() {
-        this.roomRepository.deleteAll();
+        databaseManipulator.clearRoomRepository();
+        databaseManipulator.clearUserRepository();
+        this.rooms.clear();
     }
 
     private List<RoomDto> addSomeRooms() {
@@ -68,7 +54,7 @@ public class RoomControllerTest extends AbstractTest {
         Faker faker = new Faker(new Locale("de"));
 
         for (int i = 0; i < 10; i++) {
-            roomDtos.add(getNewRoom(faker));
+            roomDtos.add(EntityGenerator.createNewRoom(faker, userDto));
         }
 
         return roomDtos;
@@ -78,28 +64,10 @@ public class RoomControllerTest extends AbstractTest {
     private void addRoomToDatabase() {
         List<RoomDto> repositoryUser = new ArrayList<>();
         for (RoomDto roomDto: this.rooms) {
-            repositoryUser.add(roomMapper.modelToDto(this.roomRepository.save(roomMapper.dtoToModel(roomDto))));
-        }
-
-        assertEquals(rooms.size(), repositoryUser.size());
-        for (int i= 0; i< rooms.size(); i++) {
-            assertEquals(rooms.get(i).getName(), repositoryUser.get(i).getName());
-            assertEquals(rooms.get(i).getUserId(), repositoryUser.get(i).getUserId());
-            assertEquals(rooms.get(i).getRoomObjects(), repositoryUser.get(i).getRoomObjects());
+            repositoryUser.add(databaseManipulator.addRoom(roomDto));
         }
 
         this.rooms = repositoryUser;
-    }
-
-    private void deleteFromDataBase() {
-
-        for (RoomDto roomDto: rooms) {
-            if (roomDto.getId() != null) {
-                this.roomRepository.deleteById(roomDto.getId());
-            }
-        }
-
-        rooms.clear();
     }
 
     @Test
@@ -108,7 +76,8 @@ public class RoomControllerTest extends AbstractTest {
         for (int i = 0; i< rooms.size(); i++) {
 
             MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(BASE_URL).contentType(MediaType.APPLICATION_JSON)
-                            .content(super.mapToJson(rooms.get(i)))
+                    .content(super.mapToJson(rooms.get(i)))
+                    .header("Authorization", "Bearer " + userDto.getToken())
             ).andReturn();
 
             int status = mvcResult.getResponse().getStatus();
@@ -123,7 +92,6 @@ public class RoomControllerTest extends AbstractTest {
             assertEquals(rooms.get(i).getRoomObjects(), roomDto.getRoomObjects());
             rooms.set(i, roomDto);
         }
-        deleteFromDataBase();
     }
 
 
@@ -133,6 +101,7 @@ public class RoomControllerTest extends AbstractTest {
 
         for (RoomDto room: rooms) {
             MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/" + room.getId())
+                    .header("Authorization", "Bearer " + userDto.getToken())
             ).andReturn();
 
             int status = mvcResult.getResponse().getStatus();
@@ -144,8 +113,6 @@ public class RoomControllerTest extends AbstractTest {
 
             assertEquals(roomDto, room);
         }
-
-        deleteFromDataBase();
     }
 
     @Test
@@ -153,6 +120,7 @@ public class RoomControllerTest extends AbstractTest {
         addRoomToDatabase();
 
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(BASE_URL)
+                .header("Authorization", "Bearer " + userDto.getToken())
         ).andReturn();
 
         int status = mvcResult.getResponse().getStatus();
@@ -168,23 +136,23 @@ public class RoomControllerTest extends AbstractTest {
         for (int i= 0; i < rooms.size(); i++) {
             assertEquals(rooms.get(i), roomDtos.get(i));
         }
-
-        deleteFromDataBase();
     }
 
     @Test
     public void deleteRooms() throws Exception {
-        List<RoomDto> before = getRoomFromDataBase();
+        List<RoomDto> before = databaseManipulator.getRooms();
         addRoomToDatabase();
 
         for (RoomDto roomDto: rooms) {
-            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(BASE_URL).content(roomDto.getId()).param("id", roomDto.getId())
+            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(BASE_URL)
+                    .content(roomDto.getId()).param("id", roomDto.getId())
+                    .header("Authorization", "Bearer " + userDto.getToken())
             ).andReturn();
 
             int status = mvcResult.getResponse().getStatus();
             assertEquals(200, status);
         }
-        List<RoomDto> after = getRoomFromDataBase();
+        List<RoomDto> after = databaseManipulator.getRooms();
 
         assertEquals(before.size(), after.size());
 
@@ -192,38 +160,5 @@ public class RoomControllerTest extends AbstractTest {
             assertEquals(before.get(i), after.get(i));
         }
 
-    }
-
-    private List<RoomDto> getRoomFromDataBase() {
-        return roomMapper.modelToDto(this.roomRepository.findAll());
-    }
-
-
-    private RoomDto getNewRoom(Faker faker) {
-        RoomDto roomDto = new RoomDto();
-
-        roomDto.setName(faker.funnyName().name());
-        User user = this.userRepository.save(userMapper.dtoToModel(getNewUser(faker)));
-        roomDto.setUserId(user.getId());
-        roomDto.setRoomObjects(new ArrayList<>());
-        roomDto.setCreatedAt(Timestamp.from(Instant.now().truncatedTo(ChronoUnit.SECONDS)));
-
-        return roomDto;
-    }
-
-    private UserDto getNewUser(Faker faker) {
-        UserDto userDto = new UserDto();
-        userDto.setRole(RoleDto.USER);
-
-        String firstName = faker.name().firstName();
-        String lastName = faker.name().lastName();
-
-        userDto.setEmail(firstName + "." + lastName +  "@kit.edu");
-        userDto.setFirstName(firstName);
-        userDto.setLastName(lastName);
-        userDto.setPassword(faker.crypto().md5());
-        userDto.setCreatedAt(Timestamp.from(Instant.now().truncatedTo(ChronoUnit.SECONDS)));
-
-        return userDto;
     }
 }
