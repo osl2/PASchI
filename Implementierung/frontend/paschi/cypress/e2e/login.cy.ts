f1aaa07696e1ae42babb9c1c892570958be1b8a5
David <uopmx@student.kit.edu>
2023-03-11 18:39:36 +0100

    Tests added

--- Implementierung/frontend/paschi/cypress/e2e/login.cy.ts
+++ Implementierung/frontend/paschi/cypress/e2e/login.cy.ts
@@ -1,21 +1,30 @@
 ///<reference types="cypress" />
-const user = { email: "admin@kit.edu", password: "admin" };
+const admin = { email: "admin@kit.edu", password: "admin" };
+const user = { firstName: "Testvorname", lastName: "Testnachname", email: "test@kit.edu", password: "12345678" };
 
 describe("Login test", () => {
-  it("tests input of email and password and successful login", () => {
+  it("tests input of email and password and successful admin- login", () => {
+    cy.visit("/login");
+    cy.adminLogin(admin.email, admin.password);
+    cy.logOut();
+  });
+  it("tests input of email and password and successful user- login", () => {
+    cy.visit("/login");
+    cy.userLogin(user.email, user.password);
+    cy.logOut();
+  });
+  it("tests input of email and password and unsuccessful admin- login", () => {
+    cy.visit("/login");
+    cy.get("input[name='email']").type(admin.email);
+    cy.get("input[name='password']").type("incorrect password");
+    cy.get("button[type=submit]").click();
+    cy.url().should("include", "/login");
+  });
+  it("tests input of email and password and unsuccessful user- login", () => {
     cy.visit("/login");
     cy.get("input[name='email']").type(user.email);
-    cy.get("input[name='password']").type(user.password);
+    cy.get("input[name='password']").type("incorrect password");
     cy.get("button[type=submit]").click();
-    cy.url().should("include", "/admin");
-    cy.contains("Akzeptierte Accounts");
-    cy.contains("Accountanträge");
-    cy.contains("Administratorseite");
-    cy.contains("Abmelden");
-    cy.get("button").contains("Abmelden").click();
     cy.url().should("include", "/login");
-    cy.login();
-
-
   });
 });
--- Implementierung/frontend/paschi/cypress/support/commands.ts
+++ Implementierung/frontend/paschi/cypress/support/commands.ts
@@ -10,29 +10,63 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+// Cypress.Commands.add("login", (email, password) => { ... })
 //
 //
 // -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
 //
 //
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add('login', (label) => {
-  cy.visit("/login");
-  cy.get("input[name='email']").type(user.email);
-  cy.get("input[name='password']").type(user.password);
+// -- This is will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// Must be declared global to be detected by typescript (allows import/export)
+// eslint-disable @typescript/interface-name
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable<Subject> {
+      adminLogin(eMail: string, password: string): void;
+      userLogin(eMail: string, password: string): void;
+      logOut(): void;
+    }
+  }
+}
+
+Cypress.Commands.add('adminLogin', (email, password) => {
+  cy.url().should("include", "/login");
+  cy.get("input[name='email']").type(email);
+  cy.get("input[name='password']").type(password);
   cy.get("button[type=submit]").click();
   cy.url().should("include", "/admin");
   cy.contains("Akzeptierte Accounts");
   cy.contains("Accountanträge");
   cy.contains("Administratorseite");
   cy.contains("Abmelden");
-  cy.get("button").contains("Abmelden").click();
-  cy.url().should("include", "/login");})
+});
+Cypress.Commands.add('userLogin', (email, password) => {
+  cy.url().should("include", "/login");
+  cy.get("input[name='email']").type(email);
+  cy.get("input[name='password']").type(password);
+  cy.get("button[type=submit]").click();
+  cy.url().should("include", "/dashboard");
+  cy.contains("Kürzlich verwendete Kurse");
+  cy.contains("Letzte Sitzungen");
+  cy.contains("Abmelden");
+  cy.contains("Alle anzeigen");
+});
 
+Cypress.Commands.add('logOut', () => {
+  cy.contains("Abmelden");
+  cy.get("button").contains("Abmelden").click();
+  cy.url().should("include", "/login");
+});
+
+
+
+// Convert this to a module instead of script (allows import/export)
+export {}
