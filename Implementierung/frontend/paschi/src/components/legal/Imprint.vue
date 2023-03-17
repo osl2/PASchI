ce00f723143e52efae34cbf925a21bac1a135e64
Cian Payne <cian.payne@student.kit.edu>
2023-03-17 20:17:59 +0100

    Add data protection and imprint components

---
+++ Implementierung/frontend/paschi/src/components/legal/Imprint.vue
@@ -0,0 +1,32 @@
+<template>
+  <NavigationBar>
+    <v-app-bar-title> Impressum </v-app-bar-title>
+  </NavigationBar>
+  <SideMenu />
+  <v-main class="v-row justify-center">
+    <v-card class="mt-5 v-col" max-width="1000">
+      <v-card-title class="text-h5 text-center text-indigo-darken-4 text-wrap">
+        <!--- Hier kommt der Titel hin ---->
+      </v-card-title>
+      <v-card-text>
+        <!--- Hier kommt der Text hin --->
+      </v-card-text>
+    </v-card>
+  </v-main>
+</template>
+
+<script lang="ts">
+import NavigationBar from "@/components/navigation/NavigationBar.vue";
+import SideMenu from "@/components/navigation/SideMenu.vue";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "Imprint",
+  components: { SideMenu, NavigationBar },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
