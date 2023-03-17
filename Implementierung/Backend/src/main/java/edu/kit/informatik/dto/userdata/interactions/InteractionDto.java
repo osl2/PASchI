8ba41a36ccb99d52b6832a5b9ba33e2ec554b9d5
Florian Knechtel <fj.knechtel@gmail.com>
2023-03-17 19:31:17 +0100

    Update Comparable & @EqualsAndHashcode-Annotation

--- Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/interactions/InteractionDto.java
+++ Implementierung/Backend/src/main/java/edu/kit/informatik/dto/userdata/interactions/InteractionDto.java
@@ -3,11 +3,13 @@ package edu.kit.informatik.dto.userdata.interactions;
 
 import edu.kit.informatik.model.userdata.interactions.Interaction;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
+import java.util.UUID;
 
 /**
  * Data-Transfer-Object zur Klasse {@link Interaction}
@@ -16,11 +18,12 @@ import java.sql.Timestamp;
  * @author ugqbo
  * @version 1.0
  */
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class InteractionDto {
+public class InteractionDto implements Comparable<InteractionDto> {
 
     private String id;
     private String userId;
@@ -31,4 +34,12 @@ public class InteractionDto {
     private String categoryId;
     private Timestamp createdAt;
     private Timestamp updatedAt;
+
+    @Override
+    public int compareTo(InteractionDto o) {
+        UUID thisInteraction = UUID.fromString(this.id);
+        UUID oInteraction = UUID.fromString(o.getId());
+
+        return  thisInteraction.compareTo(oInteraction);
+    }
 }
