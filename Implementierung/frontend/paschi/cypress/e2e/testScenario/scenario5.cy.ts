fbb59282f54df2c5dfbb062afa5c7b34c64dd812
David <uopmx@student.kit.edu>
2023-03-17 21:13:08 +0100

    Scenario added

---
+++ Implementierung/frontend/paschi/cypress/e2e/testScenario/scenario5.cy.ts
@@ -0,0 +1,47 @@
+///<reference types="cypress" />
+import {user} from "../../support/commands";
+describe("add course test", () => {
+  before(() => {
+    cy.resetTestAccount();
+  })
+  it("tests scenario 3", () => {
+    const course = { name: "Klasse 10b", subject: "Informatik" };
+    const students = Array.of(
+      { firstName: "Anna", lastName: "Bolika" },
+      { firstName: "Reiner", lastName: "Zufall" },
+      { firstName: "Polly", lastName: "Zist" },
+      { firstName: "Frank", lastName: "Reich" },
+      { firstName: "Lang", lastName: "Fall" },
+      { firstName: "Rosa", lastName: "Wurst" },
+      { firstName: "Hans", lastName: "Maul" },
+      { firstName: "Kai", lastName: "Ser" },
+      { firstName: "Heide", lastName: "Witzka" },
+      { firstName: "Axel", lastName: "Axel" },
+      { firstName: "Peter", lastName: "Petersilie" },
+      { firstName: "Mary", lastName: "Huana" },
+      { firstName: "Ellen", lastName: "Lang" },
+      { firstName: "Harry", lastName: "Bo" },
+      { firstName: "Jo", lastName: "Ghurt" }
+    );
+    cy.visit("/login");
+    cy.userLogin(user.email, user.password);
+    cy.url().should("include", "/dashboard");
+    cy.sideMenuTo("courses");
+    cy.addCourse(course.name, course.subject);
+    cy.sideMenuTo("students");
+    students.forEach((student)=>{
+      cy.addStudent(student.firstName, student.lastName);
+    });
+    students.forEach((student)=>{
+      cy.get("[name=student]").contains(student.firstName+" "+student.lastName);
+    });
+    cy.sideMenuTo("courses");
+    cy.get(".v-list-item[name=course]").click();
+    cy.get("button[name=addStudent]").click();
+    students.forEach((student) => {
+      cy.get(".v-list-item").contains(student.firstName).and("contain", student.lastName).last().click()
+    })
+    
+    cy.desktopLogOut();
+  });
+});
